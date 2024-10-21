d3f735d9473ce9fb2f9fd9dc09b5022a000559d2
malloc-realloc <malloc_realloc_calloc@outlook.com>
2024-10-21 15:47:26 +0800

    checkpoint: check if-then. check shortOpt now can look into father env

--- checker.ts
+++ checker.ts
@@ -16,20 +16,41 @@ export namespace checker {
     if (!facts) return RType.Error;
 
     for (const storedFact of facts) {
-      const freeToFixMap = new Map<string, string>();
-      storedFact.vars.forEach((lst, i) =>
-        lst.forEach((s, j) => freeToFixMap.set(s, opt.params[i][j]))
-      );
-
+      /**
+       * Main Logic of Checking Steps:
+       * 1. literally correct
+       * 2. req correct
+       * 2.1 locally correct
+       * 2.2 correctness is given by father
+       */
       if (
         storedFact.vars.every((ls, i) =>
           ls.every((s, j) => checkSingleVar(s, opt.params[i][j]))
         )
       ) {
+        /**
+         * check current opt by replacing potential hashed var with current var
+         */
+        //! I think the following fixing is buggy
+        const freeToFixMap = new Map<string, string>();
+        storedFact.vars.forEach((lst, i) =>
+          lst.forEach((s, j) => freeToFixMap.set(s, opt.params[i][j]))
+        );
+
         if (
-          storedFact.req.every(
-            (e) => checker.check(env, fixFree(e, freeToFixMap)) == RType.True
-          )
+          storedFact.req.every((e) => {
+            const out = checker.check(env, fixFree(e, freeToFixMap));
+            switch (out) {
+              case RType.True:
+                return true;
+              case RType.Unknown: {
+                if (env.father) return checkShortOpt(env.father, opt);
+                else return false;
+              }
+              default:
+                return false;
+            }
+          })
         ) {
           env.messages.push(`${opt} is true, by ${storedFact}`);
           return RType.True;
@@ -53,6 +74,30 @@ export namespace checker {
       return checkBy(env, node);
     }
 
+    return RType.Error;
+  }
+
+  /**
+   * Steps
+   * 1. open a new Env
+   * 2. emit var and req defined in if-then to new env
+   * 3. check onlyIfs of if-then
+   */
+  export function checkIfThen(env: L_Env, node: IfThenNode): RType {
+    const newEnv = new L_Env(env);
+    newEnv.declareNewVar(node.freeVars);
+    executor.knowExec(newEnv, new KnowNode(node.req));
+
+    for (const fact of node.onlyIfs) {
+      const out = check(newEnv, fact);
+      if (out === RType.Error) return RType.Error;
+      else if ([RType.False, RType.Unknown].includes(out)) return out;
+    }
+
+    return RType.True;
+  }
+
+  function checkBy(env: L_Env, node: ByNode): RType {
     return RType.Error;
   }
 
@@ -80,29 +125,5 @@ export namespace checker {
       );
     }
     throw Error("fact should be if-then or shortOpt");
-  }
-
-  /**
-   * Steps
-   * 1. open a new Env
-   * 2. introduce var defined in if-then to new env
-   * 3. check onlyIfs of if-then
-   */
-  export function checkIfThen(env: L_Env, node: IfThenNode): RType {
-    const newEnv = new L_Env(env);
-    newEnv.declareNewVar(node.freeVars);
-    executor.knowExec(newEnv, new KnowNode(node.req));
-
-    for (const fact of node.onlyIfs) {
-      const out = check(newEnv, fact);
-      if (out === RType.Error) return RType.Error;
-      else if (out === RType.False) return RType.False;
-    }
-
-    return RType.True;
-  }
-
-  function checkBy(env: L_Env, node: ByNode): RType {
-    return RType.Error;
   }
 }
