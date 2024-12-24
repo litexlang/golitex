5a6954f704189d81c1610517b17cbfb165da5152
malloc-realloc <malloc_realloc_free@outlook.com>
2024-12-24 16:12:39 +0800

    use algo written in ts to check \++{n} = n+1

--- litex_lib/natural_number.ts
+++ litex_lib/natural_number.ts
@@ -39,42 +39,55 @@ export function addDefinition(env: L_Env, opt: OptNode): L_Out {
 
     return undefined;
   }
-
-  function addStrings(num1: string, num2: string): string {
-    let carry = "0";
-    let result = [];
-    let i = num1.length - 1;
-    let j = num2.length - 1;
-
-    const addSingleDigits = (
-      d1: string,
-      d2: string,
-      carry: string
-    ): [string, string] => {
-      const digitMap = "0123456789";
-      let sum =
-        digitMap.indexOf(d1) + digitMap.indexOf(d2) + digitMap.indexOf(carry);
-      const newCarry = digitMap[Math.floor(sum / 10)];
-      const digit = digitMap[sum % 10];
-      return [digit, newCarry];
-    };
-
-    while (i >= 0 || j >= 0 || carry !== "0") {
-      const digit1 = i >= 0 ? num1[i] : "0";
-      const digit2 = j >= 0 ? num2[j] : "0";
-
-      const [sumDigit, newCarry] = addSingleDigits(digit1, digit2, carry);
-      result.push(sumDigit);
-      carry = newCarry;
-
-      i--;
-      j--;
-    }
-
-    return result.reverse().join("");
-  }
 }
 
-function plusplusEqualsPlusOne(env: L_Env, opt: OptNode): L_Out {
+function addStrings(num1: string, num2: string): string {
+  let carry = "0";
+  let result = [];
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+
+  const addSingleDigits = (
+    d1: string,
+    d2: string,
+    carry: string
+  ): [string, string] => {
+    const digitMap = "0123456789";
+    let sum =
+      digitMap.indexOf(d1) + digitMap.indexOf(d2) + digitMap.indexOf(carry);
+    const newCarry = digitMap[Math.floor(sum / 10)];
+    const digit = digitMap[sum % 10];
+    return [digit, newCarry];
+  };
+
+  while (i >= 0 || j >= 0 || carry !== "0") {
+    const digit1 = i >= 0 ? num1[i] : "0";
+    const digit2 = j >= 0 ? num2[j] : "0";
+
+    const [sumDigit, newCarry] = addSingleDigits(digit1, digit2, carry);
+    result.push(sumDigit);
+    carry = newCarry;
+
+    i--;
+    j--;
+  }
+
+  return result.reverse().join("");
+}
+
+export function plusplusEqualsPlusOne(env: L_Env, opt: OptNode): L_Out {
+  if (
+    opt.optSymbol.name === "=" &&
+    opt.vars.length === 2 &&
+    opt.vars[0] instanceof L_Composite &&
+    opt.vars[0].values[0] instanceof L_Singleton &&
+    isNaturalNumber(opt.vars[0].values[0].value) &&
+    opt.vars[1] instanceof L_Singleton &&
+    isNaturalNumber(opt.vars[1].value) &&
+    addStrings(opt.vars[0].values[0].value, "1") === opt.vars[1].value
+  ) {
+    return L_Out.True;
+  }
+
   return L_Out.Unknown;
 }
