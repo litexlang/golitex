89330bd850baeba8517dadce59a893c32429788a
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-05 14:11:41 +0800

    clean code, fix bugs

--- L_Symbols.ts
+++ L_Symbols.ts
@@ -444,3 +444,40 @@ export class L_Composite extends L_Symbol {
 }
 
 // export class FormalSymbol extends L_Singleton {}
+
+export class SymbolDeclaredChecker {
+  static check(env: L_Env, symbol: L_Symbol): void {
+    if (symbol instanceof L_Singleton) {
+      return this.checkSingleton(env, symbol);
+    } else if (symbol instanceof L_Composite) {
+      return this.checkComposite(env, symbol);
+    } else if (symbol instanceof IndexedSymbol) {
+      return this.checkIndexedSymbol(env, symbol);
+    }
+
+    throw Error();
+  }
+
+  private static errMessage(varNotDeclared: string): string {
+    return `Not Declared: ${varNotDeclared}`;
+  }
+
+  private static checkSingleton(env: L_Env, symbol: L_Singleton): void {
+    if (env.isSingletonDeclared(symbol.value)) return;
+    else throw Error(this.errMessage(symbol.value));
+  }
+
+  private static checkComposite(env: L_Env, symbol: L_Composite): void {
+    if (env.getCompositeVar(symbol.name) === undefined) return;
+
+    for (const value of symbol.values) {
+      // value.tryVarsDeclared(env);
+      this.check(env, value);
+    }
+  }
+
+  private static checkIndexedSymbol(env: L_Env, symbol: IndexedSymbol): void {
+    // symbol.given.tryVarsDeclared(env);
+    this.check(env, symbol.given);
+  }
+}
