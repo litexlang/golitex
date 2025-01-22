41b2ed64393b866869c4d52a0fa9f70e5c6a6623
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-22 18:50:57 +0800

    clean code

---
+++ parser/builtin_keywords.go
@@ -0,0 +1,11 @@
+package parser
+
+var Keywords = map[string]string{
+	"concept":  "concept",
+	"property": "property",
+	"if":       "if",
+	"opt":      "opt",
+	"fn":       "fn",
+	"local":    "local",
+	"exists":   "exists",
+}
--- parser/parse_statements.go
+++ parser/parse_statements.go
@@ -2,23 +2,30 @@ package parser
 
 import "fmt"
 
-func ParseStmt(tokenStmtBlock *TokenStmtBlock) (Stmt, error) {
+func ParseStmt(tokenStmtBlock *TokenStmt) (Stmt, error) {
 	switch tokenStmtBlock.Header[0] {
-	case "concept":
+	case Keywords["concept"]:
 		return parseConceptStmt(tokenStmtBlock)
-	case "property":
+	case Keywords["property"]:
 		return parsePropertyStmt(tokenStmtBlock)
+	case Keywords["fn"]:
+		return parseFnStmt(tokenStmtBlock)
 	default:
 		return nil, fmt.Errorf("Invalid statement: %s", tokenStmtBlock.String())
 	}
 }
 
-func parseConceptStmt(tokenStmtBlock *TokenStmtBlock) (*DefConceptStmt, error) {
+func parseConceptStmt(tokenStmtBlock *TokenStmt) (*DefConceptStmt, error) {
 	// TODO: Implement parsing logic for concept statement
 	return nil, nil
 }
 
-func parsePropertyStmt(tokenStmtBlock *TokenStmtBlock) (*DefPropertyStmt, error) {
+func parseFnStmt(tokenStmtBlock *TokenStmt) (*DefConceptStmt, error) {
+	// TODO: Implement parsing logic for concept statement
+	return nil, nil
+}
+
+func parsePropertyStmt(tokenStmtBlock *TokenStmt) (*DefPropertyStmt, error) {
 	// TODO: Implement parsing logic for property statement
 	return nil, nil
 }
