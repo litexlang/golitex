869e94b9012321b1856dbf5ea49973f336785f4b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-23 11:30:47 +0800

    parse_nodes, parse_nodes_test now takes in *int, *[]string  as parameter

--- parser/parse_nodes.go
+++ parser/parse_nodes.go
@@ -13,27 +13,70 @@ func skip(tokens *[]string, start *int, expected string) error {
 	return nil
 }
 
-func parseTypeVarPairBracket(tokens *[]string, start int) (*[]VarTypePair, int, error) {
+func parseTypeVarPairBracket(tokens *[]string, start *int) (*[]VarTypePair, error) {
 	var vals []VarTypePair
 
-	for i, token := range (*tokens)[start+1:] {
-		if token == "]" {
-			return &vals, i + 1, nil
-		}
+	// skip [
+	err := skip(tokens, start, KeyChars["["])
+	if err != nil {
+		return nil, err
+	}
 
-		v := (*tokens)[i]
-		i += 1
-		t := (*tokens)[i]
+	for *start < len(*tokens) {
+
+		v := (*tokens)[*start]
+		*start++
+		t := (*tokens)[*start]
 		typeVarPair := VarTypePair{v, t}
 
 		vals = append(vals, typeVarPair)
 
-		i += 1
-		err := skip(tokens, &i, KeyChars[","])
+		*start++
+
+		if (*tokens)[*start] == KeyChars["]"] {
+			*start++
+			return &vals, nil
+		}
+
+		err := skip(tokens, start, KeyChars[","])
 		if err != nil {
-			return nil, 0, err
+			return nil, err
 		}
 	}
 
-	return nil, 0, fmt.Errorf("expected ']', but got '%s'", (*tokens)[start])
+	return nil, fmt.Errorf("expected ']', but got '%s'", (*tokens)[*start])
+}
+
+func parseTypeVarPairBrace(tokens *[]string, start *int) (*[]VarTypePair, error) {
+	var vals []VarTypePair
+
+	// skip [
+	err := skip(tokens, start, KeyChars["("])
+	if err != nil {
+		return nil, err
+	}
+
+	for *start < len(*tokens) {
+
+		v := (*tokens)[*start]
+		*start++
+		t := (*tokens)[*start]
+		typeVarPair := VarTypePair{v, t}
+
+		vals = append(vals, typeVarPair)
+
+		*start++
+
+		if (*tokens)[*start] == KeyChars[")"] {
+			*start++
+			return &vals, nil
+		}
+
+		err := skip(tokens, start, KeyChars[","])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, fmt.Errorf("expected ')', but got '%s'", (*tokens)[*start])
 }
