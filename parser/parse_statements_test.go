88b006bac9aef3cdc85fffa0f35dfeb648b50d15
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-23 19:22:35 +0800

    parse ptystmt

--- parser/parse_statements_test.go
+++ parser/parse_statements_test.go
@@ -1,1 +1,29 @@
 package parser
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Test function for parseFuncPtyStmt
+func TestParseFuncPtyStmt(t *testing.T) {
+	tokens := []string{"$", "<", "(", "1", ",", "2", ")"}
+	tokenStmt := TokenStmt{tokens, []TokenStmt{}}
+	stmt, err := parseFuncPtyStmt(&tokenStmt)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// print stmt
+	fmt.Println(fmt.Sprintf("%v", stmt))
+}
+
+func TestParseStmt1(t *testing.T) {
+	tokens := []string{"$", "<", "(", "1", ",", "2", ")"}
+	tokenStmt := TokenStmt{tokens, []TokenStmt{}}
+	stmt, err := ParseStmt(&tokenStmt)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(fmt.Sprintf("%v", stmt))
+}
