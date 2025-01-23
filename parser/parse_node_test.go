b20b0607ec25b2cfc60076fae869eb38b56f48bc
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-23 11:30:47 +0800

    parse_nodes, parse_nodes_test now takes in *int, *[]string  as parameter

--- parser/parse_node_test.go
+++ parser/parse_node_test.go
@@ -1,1 +1,32 @@
 package parser
+
+import "testing"
+
+func TestParseTypeVarPairBracketWithoutFacts(t *testing.T) {
+	tokens := []string{"[", "g", "Group", ",", "v", "Group", "]"}
+	start := 0
+	pairs, err := parseTypeVarPairBracket(&tokens, &start)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(*pairs) != 2 || (*pairs)[0].Var != "g" || (*pairs)[0].Type != "Group" || (*pairs)[1].Var != "v" || (*pairs)[1].Type != "Group" {
+		t.Error("Expected pairs: [{g Group}, {v Group}], but got: ", pairs)
+	}
+
+	if start != len(tokens) {
+		t.Error("Expected end index: ", len(tokens), ", but got: ", start)
+	}
+}
+
+func TestParseTypeVarPairBraceWithoutFacts(t *testing.T) {
+	tokens := []string{"(", "g", "Group", ",", "v", "Group", ")"}
+
+	start := 0
+	pairs, err := parseTypeVarPairBrace(&tokens, &start)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(*pairs) != 2 || (*pairs)[0].Var != "g" || (*pairs)[0].Type != "Group" || (*pairs)[1].Var != "v" || (*pairs)[1].Type != "Group" {
+		t.Error("Expected pairs: [{g Group}, {v Group}], but got: ", pairs)
+	}
+}
