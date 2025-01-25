88b4fa7673958a427b3c3b7b92c160247439073f
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-25 16:28:18 +0800

    striterator

--- parser/parse_node_test.go
+++ parser/parse_node_test.go
@@ -5,37 +5,6 @@ import (
 	"testing"
 )
 
-func TestParseTypeVarPairBracketWithoutFacts(t *testing.T) {
-	tokens := []string{"[", "g", "Group", ",", "v", "Group", "]"}
-	start := 0
-	bracket, err := parseTypeVarPairBracket(&tokens, &start)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pairs := bracket.pairs
-	if len(pairs) != 2 || (pairs)[0].Var != "g" || (pairs)[0].Type != "Group" || (pairs)[1].Var != "v" || (pairs)[1].Type != "Group" {
-		t.Error("Expected pairs: [{g Group}, {v Group}], but got: ", pairs)
-	}
-
-	if start != len(tokens) {
-		t.Error("Expected end index: ", len(tokens), ", but got: ", start)
-	}
-}
-
-func TestParseTypeVarPairBraceWithoutFacts(t *testing.T) {
-	tokens := []string{"(", "g", "Group", ",", "v", "Group", ")"}
-
-	start := 0
-	bracket, err := parseTypeVarPairBrace(&tokens, &start)
-	if err != nil {
-		t.Fatal(err)
-	}
-	pairs := bracket.pairs
-	if len(pairs) != 2 || (pairs)[0].Var != "g" || (pairs)[0].Type != "Group" || (pairs)[1].Var != "v" || (pairs)[1].Type != "Group" {
-		t.Error("Expected pairs: [{g Group}, {v Group}], but got: ", pairs)
-	}
-}
-
 func TestParseFc(t *testing.T) {
 	tokens := []string{"f", "[", "G", ",", "B", "]", "(", "a", ",", "b", ")", "[", "G", ",", "B", "]", "(", "a", ",", "b", ")"}
 	start := 0
