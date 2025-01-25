1aa9b53950380d4a8ac602eb82e1bb361106db77
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-25 16:28:18 +0800

    striterator

--- parser/nodes.go
+++ parser/nodes.go
@@ -1,5 +1,39 @@
 package parser
 
+import "fmt"
+
+type StrIterator struct {
+	index int
+	slice []string
+}
+
+func (i *StrIterator) next() (string, error) {
+	if i.index >= len(i.slice) {
+		return "", fmt.Errorf("unexpected end of slice %v", i.slice)
+	}
+	i.index++
+	return i.slice[i.index-1], nil
+}
+
+func (i *StrIterator) skip(expected ...string) error {
+	if i.index >= len(i.slice) {
+		return fmt.Errorf("unexpected end of slice %v", i.slice)
+	}
+
+	if len(expected) == 0 {
+		i.index++
+		return nil
+	}
+
+	if i.slice[i.index] == expected[0] {
+		i.index++
+	} else {
+		return fmt.Errorf("expected '%s', but got '%s'", expected[0], i.slice[i.index])
+	}
+
+	return nil
+}
+
 type varTypePairBracket struct {
 	pairs []varTypePair
 }
