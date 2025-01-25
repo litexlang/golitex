204d6406596f82f8bcd2f7f416c0255a8a6edebf
malloc-realloc <malloc_realloc_free@outlook.com>
2025-01-25 11:35:01 +0800

    clean code

--- parser/nodes.go
+++ parser/nodes.go
@@ -13,25 +13,20 @@ type VarTypePair struct {
 type SingletonVar string
 
 type FnReturnVar struct {
-	CalledFn
+	FcFnRetVal
 }
 
-type CalledFn struct {
-	fn         FirstClassEntity
-	typeParams []FirstClassEntity
-	varParams  []FirstClassEntity
+type FcFnRetVal struct {
+	fn         FCC
+	typeParams []FCC
+	varParams  []FCC
 }
 
 type Declaration interface{}
 
 // 变量和函数是一等公民。它们的表现形式是：1. 单纯的string 2. 函数返回值
-type FirstClassEntity interface {
-}
+type FCC interface{}
 
-type FirstClassStr string
+type FCStr string
 
-type FnReturnValue struct {
-	fn         FirstClassEntity
-	typeParams []FirstClassEntity
-	varParams  []FirstClassEntity
-}
+type Var interface{}
