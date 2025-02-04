e61d7a066530c2cecd8e4483260613714b99e905
malloc-realloc <malloc_realloc_free@outlook.com>
2025-02-04 09:16:36 +0800

    clean code

--- parser/structs.go
+++ parser/structs.go
@@ -1,38 +1,6 @@
 package parser
 
-type topStmt struct {
-	stmt  Stmt
-	isPub bool
-}
-
 type typeConceptStr string
-
-type defVarStmt struct {
-	decl  fcVarDecl
-	facts []factStmt
-}
-
-type defConceptStmt struct {
-	typeVar            typeVarStr
-	conceptName        typeConceptStr
-	inherit            []typeConceptStr
-	typeVarMember      []fcVarDecl
-	typeFnMember       []fcFnDecl
-	typePropertyMember []propertyDecl
-	varMember          []fcVarDecl
-	fnMember           []fcFnDecl
-	propertyMember     []propertyDecl
-	thenFacts          []factStmt
-}
-
-type defTypeStmt struct {
-	typeVar        typeVarStr
-	conceptName    typeConceptStr
-	varMember      []fcVarDecl
-	fnMember       []fcFnDecl
-	propertyMember []propertyDecl
-	thenFacts      []factStmt
-}
 
 type fcVarDecl struct {
 	varTypePairs []fcStrTypePair
@@ -46,41 +14,6 @@ type fcFnDecl struct {
 type propertyDecl struct {
 	name string
 	tp   propertyType
-}
-
-type defPropertyStmt struct {
-	decl      propertyDecl
-	ifFacts   []factStmt
-	thenFacts []factStmt
-}
-
-type defFnStmt struct {
-	decl      fcFnDecl
-	ifFacts   []factStmt
-	thenFacts []factStmt
-}
-
-type localStmt struct {
-	statements []Stmt
-}
-
-type forallStmt struct {
-	typeParams []typeConceptPair
-	varParams  []fcStrTypePair
-	ifFacts    []factStmt
-	thenFacts  []factStmt
-}
-
-type funcPtyStmt struct {
-	isTrue bool
-	fc     Fc
-}
-
-// 1 = 2 -1 = 1 * 1, vars = [1, 2 -1, 1 * 1], opt = "="
-type relationFactStmt struct {
-	isTrue bool
-	vars   []Fc
-	opt    Fc
 }
 
 type typeConceptPair struct {
@@ -108,47 +41,7 @@ type fcFnType struct {
 	retType         fnRetType
 }
 
-// 需要让 property 不能是 fc type
 type propertyType struct {
 	typeParams []typeConceptPair
-	varParams  []fcStrTypePair // TODO not fcType!
-}
-
-type claimStmt struct {
-	toCheck []factStmt
-	proof   []Stmt
-}
-
-type defuseStmt struct {
-	name     string
-	variable Fc
-}
-
-type knowStmt struct {
-	facts []factStmt
-}
-
-type defExistStmt struct {
-	decl      propertyDecl
-	ifFacts   []factStmt
-	member    []fnRetTypeMemberDecl
-	thenFacts []factStmt
-}
-
-type haveStmt struct {
-	propertyStmt notFactStmt
-	member       []string
-}
-
-type defMemberStmt struct {
-	typeConcept typeConceptPair
-	varType     fcStrTypePair
-	member      fcDecl
-	facts       []factStmt
-}
-
-type defTypeMemberStmt struct {
-	typeConcept typeConceptPair
-	member      fcDecl
-	facts       []factStmt
+	varParams  []fcStrTypePair
 }
