4a9d9f8ac1d0895635a8ed92e19fd445d7d7720e
malloc-realloc <malloc_realloc_free@outlook.com>
2025-02-06 11:15:24 +0800

    fix obscure bugs

--- memory/memory_test.go
+++ memory/memory_test.go
@@ -38,7 +38,7 @@ f(t) is red
 func TestGetMemKey2(t *testing.T) {
 	code := `
 f(t)
-f(h[a](b).g[c](d).t)
+h[a](b).g[c](d).t
 `
 	tokenBlocks, err := parser.ParseSourceCodeGetTokenBlock(code)
 	if err != nil {
@@ -47,6 +47,7 @@ f(h[a](b).g[c](d).t)
 
 	for _, block := range *tokenBlocks {
 		it := block.Header
+		// 单独把node拿出来parse可能会有问题：因为我在parse时默认在parse stmt，所以你想要单独parse fc是会出错的。比如parse "h[a](b).g[c](d).t"时，我解释器会往t后面读东西以确定现在在什么位置，但如果 它 单独出现，那t就在最后了，就会 out of range。我这里的implement让 out of range 不可能，但以后不确定
 		fc, err := it.ParseFcExpr()
 		if err != nil {
 			t.Fatal(err)
