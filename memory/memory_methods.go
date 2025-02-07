b9f95309bae6fde5aeac350875e33b1e97cb6981
malloc-realloc <malloc_realloc_free@outlook.com>
2025-02-07 19:41:47 +0800

    Var memory set

--- memory/memory_methods.go
+++ memory/memory_methods.go
@@ -1,9 +1,21 @@
 package memory
 
+import "golitex/parser"
+
 func (mem *VarMemory) Get(s string) (*VarMemoryEntry, bool) {
 	ret, ok := mem.entries[s]
 	if !ok {
 		return nil, false
 	}
 	return &ret, true
+}
+
+func (mem *VarMemory) Set(pair *parser.FcVarDeclPair) (*VarMemoryEntry, error) {
+	toStore := VarMemoryEntry{
+		pair.Tp,
+		[]parser.FcVarType{pair.Tp},
+	}
+	mem.entries[pair.Var] = toStore
+
+	return &toStore, nil
 }
