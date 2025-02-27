f9c1a17630161428702648aa75fd6e2f1a63879a
malloc-realloc <malloc_realloc_free@outlook.com>
2025-02-27 11:52:43 +0800

    memory

---
+++ litex_memory/fact.go
@@ -0,0 +1,71 @@
+package litexmemory
+
+import (
+	parser "golitex/litex_parser"
+)
+
+type FcEnum uint8
+
+const (
+	FcStrEnum FcEnum = iota
+	FcFnRetValueEnum
+	FcFnCallChainEnum
+)
+
+type FcEnumsFactsKVPair struct {
+	Types []FcEnum
+	Facts PropFactValues
+}
+
+// Define type PropName to signify functionality of a string variable
+type PropName string
+
+type SpecFactMemory struct {
+	PropFactsMap map[PropName]PropFacts
+}
+
+type PropFacts []FcEnumsFactsKVPair
+
+// TODO
+type PropFactValues interface {
+	propFactValues()
+}
+
+type SpecMemoryFact struct {
+}
+
+type CondFactMemory struct {
+	KVs map[PropName]CondFactMemEntry
+}
+
+type CondFactMemEntry struct{ Facts []CondFactMemFact }
+
+type CondFactMemFact struct {
+	cond *[]parser.FactStmt
+	then parser.FactStmt
+}
+
+type UniFactMemory struct {
+	Entires map[PropName]UniFactMemEntry
+}
+
+type UniFactMemEntry struct{ Facts []UniMemFact }
+
+type UniMemFact struct {
+	typeParams *[]parser.TypeConceptPair
+	varParams  *[]parser.StrTypePair
+	cond       *[]parser.FactStmt
+	then       *[]parser.SpecFactStmt
+}
+
+func NewSpecFactMemory() *SpecFactMemory {
+	return &SpecFactMemory{PropFactsMap: map[PropName]PropFacts{}}
+}
+
+func NewUniFactMemory() *UniFactMemory {
+	return &UniFactMemory{map[PropName]UniFactMemEntry{}}
+}
+
+func NewCondFactMemory() *CondFactMemory {
+	return &CondFactMemory{KVs: map[PropName]CondFactMemEntry{}}
+}
