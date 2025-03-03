4f2fd3880bde380956ced839f99eb4ec4a9bb840
malloc-realloc <malloc_realloc_free@outlook.com>
2025-03-03 14:57:21 +0800

    big checkpoint: can store new spec func fact and check spec fact based on known spec func fact

--- litex_executor/executor_test.go
+++ litex_executor/executor_test.go
@@ -27,3 +27,53 @@ func TestStoreNewVar(t *testing.T) {
 	entry, _ := env.VarMemory.Get("a")
 	println(string(entry.Tp.Value.(parser.FcVarTypeStrValue)))
 }
+
+func TestKnow(t *testing.T) {
+	code := `know $p(a)`
+	statements, err := parser.ParseSourceCode(code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	env := memory.NewEnv()
+	executor := Executor{env, []string{}, ExecTrue}
+	for _, topStmt := range *statements {
+		err := executor.TopLevelStmt(&topStmt)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(executor.output)
+		fmt.Println(executor.message)
+	}
+}
+
+func TestVerifier(t *testing.T) {
+	code := `know $p(a)`
+	statements, err := parser.ParseSourceCode(code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	env := memory.NewEnv()
+	executor := Executor{env, []string{}, ExecTrue}
+	for _, topStmt := range *statements {
+		err := executor.TopLevelStmt(&topStmt)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(executor.output)
+		fmt.Println(executor.message)
+	}
+
+	testCodes := "$p(b)\n$p(a)"
+	testStatements, err := parser.ParseSourceCode(testCodes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, testCode := range *testStatements {
+		err := executor.TopLevelStmt(&testCode)
+		if err != nil {
+			t.Fatal(err)
+		}
+		executor.println()
+	}
+}
--- litex_parser/parser_structs_basic_methods.go
+++ litex_parser/parser_structs_basic_methods.go
@@ -7,5 +7,9 @@ func (stmt *KnowStmt) String() string {
 }
 
 func (stmt *FuncFactStmt) String() string {
-	return fmt.Sprintf("%s %s", stmt.IsTrue, stmt.Fc)
+	if stmt.IsTrue {
+		return fmt.Sprintf("%v", stmt.Fc)
+	} else {
+		return fmt.Sprintf("not %v", stmt.Fc)
+	}
 }
