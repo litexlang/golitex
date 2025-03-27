3d7939671098bb48c43e9d340c37cfb263a71ec8
malloc-realloc <malloc_realloc_free@outlook.com>
2025-03-27 21:03:40 +0800

    clean code

--- litex_verifier/verifier_messages.go
+++ litex_verifier/verifier_messages.go
@@ -3,11 +3,15 @@ package litexverifier
 import "fmt"
 
 func (e *Verifier) successMsg(stmtString, storedStmtString string) {
-	message := fmt.Sprintf("[true]\n%v\n[verified by]\n%v", stmtString, storedStmtString)
-	*e.Message = append(*e.Message, message) // 新消息插在后面
+	if e.round1() {
+		message := fmt.Sprintf("[true]\n%v\n[verified by]\n%v", stmtString, storedStmtString)
+		*e.Message = append(*e.Message, message) // 新消息插在后面
+	}
 }
 
 func (e *Verifier) unknownMsg(format string, args ...any) {
-	message := fmt.Sprintf(format, args...)
-	*e.Message = append(*e.Message, message) // 新消息插在后面
+	if e.round1() {
+		message := fmt.Sprintf(format, args...)
+		*e.Message = append(*e.Message, message) // 新消息插在后面
+	}
 }
