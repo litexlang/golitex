34a4f194e64c2af21df536aae12b4a886e4396d6
malloc-realloc <malloc_realloc_free@outlook.com>
2025-03-28 09:22:45 +0800

    start to implementverify fc equal. spec almost done

--- litex_verifier/verify_fc_equal_spec.go
+++ litex_verifier/verify_fc_equal_spec.go
@@ -1,6 +1,7 @@
 package litexverifier
 
 import (
+	"fmt"
 	cmp "golitex/litex_comparator"
 	parser "golitex/litex_parser"
 )
@@ -21,6 +22,9 @@ func (verifier *Verifier) fcEqual(left, right parser.Fc, addRound bool) error {
 
 func (ver *Verifier) fcEqualSpec(left, right parser.Fc) error {
 	err := ver.fcEqualSpecDirectly(left, right)
+	if err != nil {
+		return err
+	}
 
 	cmpRet, fcEnum, err := cmp.CmpFcType(left, right)
 	if err != nil {
@@ -41,6 +45,15 @@ func (ver *Verifier) fcEqualSpec(left, right parser.Fc) error {
 }
 
 func (ver *Verifier) fcEqualSpecDirectly(left, right parser.Fc) error {
+	// 完全一样
+	cmpRet, err := cmp.CmpFc(left, right)
+	if err != nil {
+		return err
+	}
+	if cmpRet == 0 {
+		return nil
+	}
+
 	// 如果之前显式地说过 left = right，那就直接成立
 	return nil
 }
@@ -50,5 +63,41 @@ func (verifier *Verifier) fcAtomEqualSpec(left, right *parser.FcAtom) error {
 }
 
 func (verifier *Verifier) fcFnCallPipeEqualSpec(left, right *parser.FcFnCallPipe) error {
+	// 如果两个函数的名字一样，每个参数都一样，那也行
+	ret, err := cmp.CmpFc(&left.FnHead, &right.FnHead)
+	if err != nil {
+		return err
+	}
+
+	if ret != 0 {
+		return nil
+	}
+
+	if len(left.CallPipe) != len(right.CallPipe) {
+		return nil
+	}
+
+	for i := 0; i < len(left.CallPipe); i++ {
+		if len(left.CallPipe[i].Params) != len(right.CallPipe[i].Params) {
+			return nil
+		}
+
+		for j := 0; j < len(left.CallPipe[i].Params); j++ {
+			err := verifier.fcEqualSpec(left.CallPipe[i].Params[j], right.CallPipe[i].Params[j])
+			if err != nil {
+				return err
+			}
+			if !verifier.true() {
+				return nil
+			}
+		}
+	}
+
+	if verifier.round1() {
+		verifier.successMsg(fmt.Sprintf("%v = %v", left.String(), right.String()), "use known facts")
+	} else {
+		verifier.successNoMsg()
+	}
+
 	return nil
 }
