8639a73a6a7953902c3aa629efca6f88245ce744
malloc-realloc <malloc_realloc_free@outlook.com>
2025-03-31 09:05:12 +0800

    prop_prop verify

---
+++ litex_verifier/verify_prop_prop.go
@@ -0,0 +1,15 @@
+package litexverifier
+
+import (
+	parser "golitex/litex_parser"
+)
+
+func (ver *Verifier) IsPropProp(stmt *parser.SpecFactStmt) (bool, error) {
+	// TODO
+	return false, nil
+}
+
+func (ver *Verifier) PropPropFact(stmt *parser.SpecFactStmt) (bool, error) {
+	// TODO	判断一下传入进来的stmt是不是prop prop，就像数学归纳法这种。prop prop的特点是，它是prop，参数列表里也有prop。如果是的话，那就用其他方式来验证
+	return false, nil
+}
