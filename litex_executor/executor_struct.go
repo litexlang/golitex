13b1df73381ed0f21d3b54f3e87e2958c6b7f55e
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-03 11:03:40 +0800

    refactor: now verifier and executor only has field env

--- litex_executor/executor_struct.go
+++ litex_executor/executor_struct.go
@@ -4,26 +4,18 @@ import (
 	env "golitex/litex_env"
 )
 
-type ExecOutput uint8
-
-const (
-	execTrue ExecOutput = iota
-	execUnknown
-	execError
-)
-
 type Executor struct {
 	env *env.Env
 	// parent  *Executor
 	// msgs   []string
-	output ExecOutput
+	// output ExecOutput
 }
 
 func NewExecutor(curEnv *env.Env) *Executor {
 	if curEnv == nil {
-		return &Executor{env: env.NewEnv(nil, nil), output: execUnknown}
+		return &Executor{env: env.NewEnv(nil, nil)}
 	} else {
-		return &Executor{env: curEnv, output: execUnknown}
+		return &Executor{env: curEnv}
 	}
 }
 
@@ -31,17 +23,15 @@ func (e *Executor) newEnv() {
 	e.env = env.NewEnv(e.env, nil)
 }
 
-func (e *Executor) deleteEnv() {
+func (e *Executor) deleteEnvAndRetainMsg() {
+	for _, msg := range e.env.Msgs {
+		e.env.Parent.NewMsg(msg)
+	}
 	e.env = e.env.Parent
 }
 
 func (e *Executor) clearMsgAndOutput() {
 	e.env.Msgs = []string{}
-	e.output = execUnknown
-}
-
-func (e *Executor) true() bool {
-	return e.output == execTrue
 }
 
 func (e *Executor) getMsgs() []string {
