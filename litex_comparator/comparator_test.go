34ea241b958cfe5f667870f773da411fb0d3d922
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-06 10:31:03 +0800

    start to impl compare_number, eval number builtin operation

--- litex_comparator/comparator_test.go
+++ litex_comparator/comparator_test.go
@@ -1,1 +1,37 @@
-package litexcomparator_test
+package litexcomparator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFcEval(t *testing.T) {
+	// 测试用例
+	testCases := []struct {
+		a, b, expected string
+	}{
+		{"123.45", "67.89", "191.34"},
+		{"0.1", "0.2", "0.3"},
+		{"99999999999999999999.99999999999999999999", "0.00000000000000000001", "100000000000000000000.00000000000000000000"},
+		{"1.0000000000000000000000000000000000000001", "2.0000000000000000000000000000000000000002", "3.0000000000000000000000000000000000000003"},
+	}
+
+	for _, tc := range testCases {
+		result := addBigFloat(tc.a, tc.b)
+		fmt.Printf("%s + %s = %s (期望: %s) ", tc.a, tc.b, result, tc.expected)
+		if CompareBigFloat(result, tc.expected) == 0 {
+			fmt.Println("✓")
+		} else {
+			fmt.Println("✗")
+		}
+	}
+
+	fmt.Println(CompareBigFloat("1.23", "1.23000"))    // 0
+	fmt.Println(CompareBigFloat("1.23", "1.24"))       // -1
+	fmt.Println(CompareBigFloat("123.456", "123.456")) // 0
+	fmt.Println(CompareBigFloat("123.456", "123.455")) // 1
+	fmt.Println(CompareBigFloat("00001.000", "1"))     // 0
+	fmt.Println(CompareBigFloat("10.00001", "10"))     // 1
+	fmt.Println(CompareBigFloat("9.9999", "10"))       // -1
+
+}
---
+++ litex_verifier/verify_fc_equal_builtin.go
@@ -0,0 +1,12 @@
+package litexverifier
+
+import parser "golitex/litex_parser"
+
+func (ver *Verifier) fcEqualBuiltin(left, right parser.Fc) (bool, error) {
+	// Case1: 二者都是 Number 上进行+-*/^
+	
+
+	return false, nil
+}
+
+func 
