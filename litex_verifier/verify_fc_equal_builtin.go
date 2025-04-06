116c1a87ca1054dded08d9fd32b679e4adf3455a
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-06 10:54:34 +0800

    start to impl compare_number, eval number builtin operation

--- litex_verifier/verify_fc_equal_builtin.go
+++ litex_verifier/verify_fc_equal_builtin.go
@@ -4,9 +4,6 @@ import parser "golitex/litex_parser"
 
 func (ver *Verifier) fcEqualBuiltin(left, right parser.Fc) (bool, error) {
 	// Case1: 二者都是 Number 上进行+-*/^
-	
 
 	return false, nil
 }
-
-func 
