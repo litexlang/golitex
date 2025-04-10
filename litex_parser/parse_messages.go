77098666d2ae9e6255c061c54d523f6d1346a7f2
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-10 17:42:25 +0800

    add: max allowed nested forall in parse time

--- litex_parser/parse_messages.go
+++ litex_parser/parse_messages.go
@@ -15,3 +15,12 @@ func thenFactMustSpecMsg(curStmt *tokenBlock, err error) error {
 	}
 	return &parseTimeErr{err, *curStmt}
 }
+
+func uniDomFactMoreThanOneLayerUniFactErrMsg(err error, curStmt *tokenBlock) error {
+	startStr := curStmt.header.strAtIndex(0)
+	if startStr == glob.KeywordForall {
+		return &parseTimeErr{fmt.Errorf("dom fact in universal fact should only be a universal fact with a universal fact as dom fact"), *curStmt}
+	}
+
+	return &parseTimeErr{err, *curStmt}
+}
