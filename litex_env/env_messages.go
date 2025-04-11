906e844b891fe347b20c42c30cb50b23f76993e5
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-11 17:23:31 +0800

    clean

--- litex_env/env_messages.go
+++ litex_env/env_messages.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func nameDeclaredMsg(pkgName string, name string, keyword string) error {
 	if pkgName == "" {
-		return fmt.Errorf("%s already exists in current scope, it is a %s\n", name, keyword)
+		return fmt.Errorf("%s already exists in current scope, it is a %s", name, keyword)
 	} else {
-		return fmt.Errorf("%s already exists in %s package, it is a %s\n", name, pkgName, keyword)
+		return fmt.Errorf("%s already exists in %s package, it is a %s", name, pkgName, keyword)
 	}
 }
