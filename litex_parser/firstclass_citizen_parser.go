3bb9dc1f969dd671badbc1ae99a796816c6deaf7
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-12 09:38:02 +0800

    fix bug: parse () of Fc and parse fcInfixExpr bug

--- litex_parser/firstclass_citizen_parser.go
+++ litex_parser/firstclass_citizen_parser.go
@@ -86,87 +86,96 @@ func (parser *strSliceCursor) rawFcAtom() (ast.FcAtom, error) {
 	return ast.FcAtom{PkgName: fromPkg, Value: value}, nil
 }
 
-// 不考虑 uniParams
 func (parser *strSliceCursor) rawFc() (ast.Fc, error) {
-	// skip brace
-	if parser.is(glob.KeySymbolLeftBrace) {
-		parser.skip(glob.KeySymbolLeftBrace)
-		fc, err := parser.rawFc()
-		if err != nil {
-			return nil, &parserErr{err, parser}
-		}
-		err = parser.skip(glob.KeySymbolRightBrace)
-
-		if err != nil {
-			return nil, &parserErr{err, parser}
-		}
-
-		return fc, nil
+	expr, err := parser.fcInfixExpr(glob.PrecLowest)
+	if err != nil {
+		return nil, err
 	}
-
-	return parser.fcInfixExpr(glob.PrecLowest)
+	return expr, nil
 }
 
 func (parser *strSliceCursor) fcInfixExpr(currentPrec glob.BuiltinOptPrecedence) (ast.Fc, error) {
-	left, err := parser.fcUnaryExpr()
+	left, err := parser.fcPrimaryExpr()
 	if err != nil {
-		return nil, &parserErr{err, parser}
+		return nil, err
 	}
 
 	for !parser.ExceedEnd() {
 		curToken, err := parser.currentToken()
 		if err != nil {
-			return nil, err // 捕获错误并退出
+			return nil, fmt.Errorf("unexpected end of input while parsing infix expression")
 		}
 
+		// 检查是否是运算符
 		curPrec, isBinary := glob.BuiltinOptPrecedenceMap[curToken]
-		if !isBinary {
-			return nil, fmt.Errorf("unexpected token: \"%s\": it is not an builtin infix operator", curToken)
-		}
-
-		if curPrec <= currentPrec {
+		if !isBinary || curPrec <= currentPrec {
 			break
 		}
 
 		parser.skip() // 消耗运算符
 		right, err := parser.fcInfixExpr(curPrec)
 		if err != nil {
-			return nil, &parserErr{err, parser}
+			return nil, err
 		}
 
 		leftHead := ast.NewFcAtom("", curToken)
-
 		left = ast.NewFcFnPipe(
 			*leftHead,
 			[]*ast.FcFnSeg{ast.NewFcFnPipeSeg([]ast.Fc{left, right})},
-			// []*ast.FcFnPipeSeg{{Params: []ast.Fc{left, right}}},
 		)
 	}
 
 	return left, nil
 }
 
-func (parser *strSliceCursor) fcUnaryExpr() (ast.Fc, error) {
-	unaryOp, err := parser.currentToken()
-	if err != nil {
-		return nil, &parserErr{err, parser}
+func (parser *strSliceCursor) fcPrimaryExpr() (ast.Fc, error) {
+	if parser.ExceedEnd() {
+		return nil, fmt.Errorf("unexpected end of input, expected expression")
 	}
 
-	if prec, isUnary := glob.UnaryPrecedence[unaryOp]; isUnary {
-		parser.skip()
-		right, err := parser.fcInfixExpr(prec)
+	// 处理括号表达式
+	if parser.is(glob.KeySymbolLeftBrace) {
+		parser.skip(glob.KeySymbolLeftBrace)
+		if parser.ExceedEnd() {
+			return nil, fmt.Errorf("unexpected end of input after '('")
+		}
+
+		expr, err := parser.fcInfixExpr(glob.PrecLowest)
 		if err != nil {
 			return nil, err
 		}
-		return &ast.FcFn{
-			FnHead:    ast.FcAtom{Value: unaryOp},
-			ParamSegs: []*ast.FcFnSeg{{Params: []ast.Fc{right}}},
-		}, nil
-		// return ast.MakeFcFnPipe(*ast.MakeFcAtom("", unaryOp), []*ast.FcFnPipeSeg{ast.MakeFcFnPipeSeg([]ast.Fc{right})})
-	} else {
-		return parser.fcAtomAndFcFnRetAndBracedFc()
+
+		if parser.ExceedEnd() {
+			return nil, fmt.Errorf("unexpected end of input, expected ')'")
+		}
+
+		if err := parser.skip(glob.KeySymbolRightBrace); err != nil {
+			return nil, fmt.Errorf("expected ')': %v", err)
+		}
+		return expr, nil
 	}
 
+	// 处理一元运算符
+	if unaryOp, err := parser.currentToken(); err == nil {
+		if prec, isUnary := glob.UnaryPrecedence[unaryOp]; isUnary {
+			parser.skip()
+			if parser.ExceedEnd() {
+				return nil, fmt.Errorf("unexpected end of input after unary operator")
+			}
+
+			right, err := parser.fcInfixExpr(prec)
+			if err != nil {
+				return nil, err
+			}
+			return &ast.FcFn{
+				FnHead:    ast.FcAtom{Value: unaryOp},
+				ParamSegs: []*ast.FcFnSeg{{Params: []ast.Fc{right}}},
+			}, nil
+		}
+	}
+
+	// 处理基本原子表达式
+	return parser.fcAtomAndFcFnRetAndBracedFc()
 }
 
 func (parser *strSliceCursor) numberStr() (*ast.FcAtom, error) {
