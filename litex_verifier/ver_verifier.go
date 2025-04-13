a85d0efd8860cf68d7ba1403b5ec75c5acab5939
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-13 20:00:07 +0800

    add liscense header

--- litex_verifier/ver_verifier.go
+++ litex_verifier/ver_verifier.go
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2024 Jiachen Shen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Original Author: Jiachen Shen (malloc_realloc_calloc@outlook.com)
+ * Visit litexlang.org and https://github.com/litexlang/golitex for more information.
+ */
+
 // verifier 的方法的命名方式：factType+?UseWhatMemToVerify+?atEnv, 比如 RelaFactSpecAtEnv 就是说 证明 relaFact, 方法是用SpecFact，在当前环境里.
 package litex_verifier
 
