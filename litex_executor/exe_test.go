46a52141975682a038ce4ab864e93784246ca6c6
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-16 09:17:45 +0800

    checkpoint:thoughts: why I think litex is strict enough. and the balance of strictness and flexibility of current litex is great. litex does not need type thoery embedded in language level.

--- litex_executor/exe_test.go
+++ litex_executor/exe_test.go
@@ -565,7 +565,7 @@ func TestAllFactCode(t *testing.T) {
 	fmt.Printf("read file takes %v\nparsing takes %v\nexecution takes %v\n", readFileTime, parseTime, executionTime)
 }
 
-var code = readFile("../litex_code_examples/test_codes/is_condFact_necessary.lix")
+var code = readFile("../litex_code_examples/classic_examples/add_one_to_hundred.lix")
 
 func TestLastFactCode(t *testing.T) {
 	start := time.Now()
