39a5824b12a1dbb4ba8bb08df8e0581dc9463e4b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-22 11:26:41 +0800

    impl existFact verifier

--- litex_executor/exe_test.go
+++ litex_executor/exe_test.go
@@ -565,7 +565,7 @@ func TestAllFactCode(t *testing.T) {
 	fmt.Printf("read file takes %v\nparsing takes %v\nexecution takes %v\n", readFileTime, parseTime, executionTime)
 }
 
-var code = readFile("../litex_code_examples/test_codes/spec_fact.lix")
+var code = readFile("../litex_code_examples/test_codes/exist_prop.lix")
 
 func TestLastFactCode(t *testing.T) {
 	start := time.Now()
