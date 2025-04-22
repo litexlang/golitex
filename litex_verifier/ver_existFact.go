b39c7cb07615bc080f53a76c5f6d5dfe9f55be37
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-22 14:10:51 +0800

    checkpoint: verify have a $p(b)

--- litex_verifier/ver_existFact.go
+++ litex_verifier/ver_existFact.go
@@ -67,7 +67,7 @@ func (ver *Verifier) useExistPropDefProveExist(stmt *ast.SpecFactStmt, state Ver
 			return false, err
 		}
 		fixedAsSpecFact := fixed.(*ast.SpecFactStmt)
-		if proveTrue {
+		if !proveTrue {
 			fixedAsSpecFact = fixedAsSpecFact.ReverseIsTrue()
 		}
 		thenFacts = append(thenFacts, fixedAsSpecFact)
@@ -85,70 +85,3 @@ func (ver *Verifier) useExistPropDefProveExist(stmt *ast.SpecFactStmt, state Ver
 
 	return false, nil
 }
-
-// func (ver *Verifier) ExistPropFact(stmt *ast.SpecFactStmt, state VerState) (bool, error) {
-// 	if state.isSpec() {
-// 		return false, nil
-// 	}
-
-// 	var newFact *ast.SpecFactStmt
-// 	if stmt.TypeEnum == ast.TrueExist {
-// 		// // 只要已经有一个是成立的，那整个exist就是成立的
-// 		// if len(stmt.Params) != 0 {
-// 		// }
-
-// 		newFact = ast.NewSpecFactStmt(ast.TrueAtom, stmt.PropName, stmt.Params)
-
-// 		ok, err := ver.SpecFact(newFact, state)
-// 		if err != nil {
-// 			return false, err
-// 		}
-// 		if ok {
-// 			return ver.factDefer(stmt, state, true, nil, newFact.String())
-// 		}
-// 		return false, nil
-// 	} else if stmt.TypeEnum == ast.FalseExist {
-// 		// not exist 表示 forall，所以Litex的语法让涉及到的参数数量为0
-// 		if len(stmt.Params) != 0 {
-// 			return false, fmt.Errorf("exist fact with params is not supported %s", stmt.String())
-// 		}
-
-// 		propDef, ok := ver.env.PropMem.Get(stmt.PropName)
-// 		if !ok {
-// 			return false, nil
-// 		}
-
-// 		var err error = nil
-
-// 		newUniFactUsingItself := ast.NewConUniFactStmt(propDef.DefHeader.Params, propDef.DefHeader.SetParams, propDef.DomFacts, []*ast.SpecFactStmt{ast.NewSpecFactStmt(ast.FalseAtom, stmt.PropName, []ast.Fc{})})
-// 		for _, param := range propDef.DefHeader.Params {
-// 			newUniFactUsingItself.ThenFacts[0].Params = append(newUniFactUsingItself.ThenFacts[0].Params, &ast.FcAtom{PkgName: glob.EmptyPkgName, PropName: param})
-// 		}
-
-// 		ok, err = ver.FactStmt(newUniFactUsingItself, state)
-// 		if err != nil {
-// 			return false, err
-// 		}
-// 		if ok {
-// 			return ver.factDefer(stmt, state, true, nil, newUniFactUsingItself.String())
-// 		}
-
-// 		newUniFactUsingIff := ast.NewConUniFactStmt(propDef.DefHeader.Params, propDef.DefHeader.SetParams, propDef.DomFacts, []*ast.SpecFactStmt{})
-
-// 		for _, fact := range propDef.IffFacts {
-// 			newUniFactUsingIff.ThenFacts = append(newUniFactUsingIff.ThenFacts, fact.ReverseIsTrue())
-// 		}
-
-// 		ok, err = ver.FactStmt(newUniFactUsingIff, state)
-// 		if err != nil {
-// 			return false, err
-// 		}
-// 		if ok {
-// 			return ver.factDefer(stmt, state, true, nil, newUniFactUsingIff.String())
-// 		}
-
-// 		return false, nil
-// 	}
-
-// 	return false, fmt.Errorf("invalid exist fact type: %d", stmt.TypeEnum)
-// }
