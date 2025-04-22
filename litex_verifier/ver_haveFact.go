f77a4cdf74f1d5cf5a92328913803ba7e1e9c0c4
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-22 12:59:24 +0800

    use exist prop def to prove have

--- litex_verifier/ver_haveFact.go
+++ litex_verifier/ver_haveFact.go
@@ -12,6 +12,7 @@
 package litex_verifier
 
 import (
+	"fmt"
 	ast "golitex/litex_ast"
 )
 
@@ -32,5 +33,57 @@ func (ver *Verifier) HavePropFact(stmt *ast.SpecFactStmt, state VerState) (bool,
 }
 
 func (ver *Verifier) useExistPropDefProveHave(stmt *ast.SpecFactStmt, state VerState, proveTrue bool) (bool, error) {
+	propDef, ok := ver.env.ExistPropMem.Get(stmt.PropName)
+	if !ok {
+		return false, nil
+	}
+
+	freeFixmap := map[string]ast.Fc{}
+	for i, param := range propDef.Def.DefHeader.Params {
+		freeFixmap[param] = stmt.Params[i]
+	}
+
+	domFacts := []ast.FactStmt{}
+	for _, fact := range propDef.Def.DomFacts {
+		fixed, err := fact.Instantiate(freeFixmap)
+		if err != nil {
+			return false, err
+		}
+		domFacts = append(domFacts, fixed)
+	}
+
+	thenFacts := []*ast.SpecFactStmt{}
+	for _, thenFact := range propDef.Def.IffFacts {
+		fixed, err := thenFact.Instantiate(freeFixmap)
+		if err != nil {
+			return false, err
+		}
+		fixedAsSpecFact := fixed.(*ast.SpecFactStmt)
+		if proveTrue {
+			fixedAsSpecFact = fixedAsSpecFact.ReverseIsTrue()
+		}
+		thenFacts = append(thenFacts, fixedAsSpecFact)
+	}
+
+	for _, domFact := range domFacts {
+		ok, err := ver.FactStmt(domFact, state)
+		if err != nil {
+			return false, err
+		}
+		if !ok {
+			return false, fmt.Errorf("dom fact %s is not true", domFact.String())
+		}
+	}
+
+	for _, thenFact := range thenFacts {
+		ok, err := ver.FactStmt(thenFact, state)
+		if err != nil {
+			return false, err
+		}
+		if ok {
+			return ver.factDefer(stmt, state, true, nil, thenFact.String())
+		}
+	}
+
 	return false, nil
 }
