ffb3485f83f4ca2d373a6213ba7c89be80989bc0
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-23 13:30:07 +0800

    test-driven refactor

--- litex_env/env_messages.go
+++ litex_env/env_messages.go
@@ -13,10 +13,10 @@ package litex_memory
 
 import "fmt"
 
-func nameDeclaredMsg(pkgName string, name string, keyword string) error {
+func duplicateDefMsg(pkgName string, name string, keyword string) error {
 	if pkgName == "" {
-		return fmt.Errorf("%s already exists in current scope, it is a %s", name, keyword)
+		return fmt.Errorf("duplicate definition of %s, it is a %s", name, keyword)
 	} else {
-		return fmt.Errorf("%s already exists in %s package, it is a %s", name, pkgName, keyword)
+		return fmt.Errorf("duplicate definition of %s in %s package, it is a %s", name, pkgName, keyword)
 	}
 }
