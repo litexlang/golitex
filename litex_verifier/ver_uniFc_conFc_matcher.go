260f885c01d1ff78a181961246cefc5d3372f232
malloc-realloc <malloc_realloc_free@outlook.com>
2025-04-29 17:00:31 +0800

    clean

--- litex_verifier/ver_uniFc_conFc_matcher.go
+++ litex_verifier/ver_uniFc_conFc_matcher.go
@@ -16,9 +16,7 @@ import (
 	"fmt"
 	ast "golitex/litex_ast"
 	cmp "golitex/litex_comparator"
-	glob "golitex/litex_global"
 	mem "golitex/litex_memory"
-	"strings"
 )
 
 // match 函数不需要传入state: 没有any, spec 之分，也不需要打印
@@ -30,7 +28,8 @@ func (ver *Verifier) matchStoredUniSpecWithSpec(knownFact mem.StoredUniSpecFact,
 	retMap := map[string][]ast.Fc{}
 
 	for i, uniParam := range knownFact.SpecFact.Params {
-		matchMap, matched, err := ver.match_FcInFactUnderUniFact_WithConFc(uniParam, stmt.Params[i], knownFact.UniFact.Params)
+		// matchMap, matched, err := ver.match_FcInFactUnderUniFact_WithConFc(uniParam, stmt.Params[i], knownFact.UniFact.Params)
+		matchMap, matched, err := ver.match_FcInFactUnderUniFact_WithConFc(uniParam, stmt.Params[i])
 		if err != nil {
 			return nil, false, err
 		}
@@ -44,7 +43,7 @@ func (ver *Verifier) matchStoredUniSpecWithSpec(knownFact mem.StoredUniSpecFact,
 }
 
 // paramInFactUnderUniFact 可能是自由的，可能是固定的，反正它来自一个forall下面的某个specFact
-func (ver *Verifier) match_FcInFactUnderUniFact_WithConFc(fcInFactUnderUniFact ast.Fc, conFc ast.Fc, uniFactUniParams []string) (map[string][]ast.Fc, bool, error) {
+func (ver *Verifier) match_FcInFactUnderUniFact_WithConFc(fcInFactUnderUniFact ast.Fc, conFc ast.Fc) (map[string][]ast.Fc, bool, error) {
 	// 注意到，如果传入的不是fn，而是atom，那大概率是不能match上的。只有一种例外:
 	// know forall x A: $p(x *(3-2)); $p(1*1) 这时候 3 -2 要能和1对上。而 uniFunc 的对应关系，只是让自由变量去对应，不包括builtinFc的match
 	// 同时，也不能直接去CmpFcRule，因为如果输入的变量的字面量刚好是存着的自由变量的字面量，那恰好相等了，这是不行的。只能是BuiltinFc 之间相等
@@ -60,15 +59,18 @@ func (ver *Verifier) match_FcInFactUnderUniFact_WithConFc(fcInFactUnderUniFact a
 	// Safe type switching
 	switch param := fcInFactUnderUniFact.(type) {
 	case *ast.FcAtom:
-		return ver.match_FcAtomInFactUnderUniFact_ConFc(param, conFc, uniFactUniParams)
+		// return ver.match_FcAtomInFactUnderUniFact_ConFc(param, conFc, uniFactUniParams)
+		return ver.match_FcAtomInFactUnderUniFact_ConFc(param, conFc)
 	case *ast.FcFn:
-		return ver.match_FcFnInFactUnderUniFact_ConFc(param, conFc, uniFactUniParams)
+		// return ver.match_FcFnInFactUnderUniFact_ConFc(param, conFc, uniFactUniParams)
+		return ver.match_FcFnInFactUnderUniFact_ConFc(param, conFc)
 	default:
 		return nil, false, fmt.Errorf("unexpected type %T for parameter %v", param, fcInFactUnderUniFact.String())
 	}
 }
 
-func (ver *Verifier) match_FcAtomInFactUnderUniFact_ConFc(fcAtomInFactUnderUniFact *ast.FcAtom, conFc ast.Fc, uniParams []string) (map[string][]ast.Fc, bool, error) {
+// func (ver *Verifier) match_FcAtomInFactUnderUniFact_ConFc(fcAtomInFactUnderUniFact *ast.FcAtom, conFc ast.Fc, uniParams []string) (map[string][]ast.Fc, bool, error) {
+func (ver *Verifier) match_FcAtomInFactUnderUniFact_ConFc(fcAtomInFactUnderUniFact *ast.FcAtom, conFc ast.Fc) (map[string][]ast.Fc, bool, error) {
 	retMap := make(map[string][]ast.Fc)
 
 	// 不利用查prefix的方式来确定涉及到的param是不是 uni
@@ -78,7 +80,7 @@ func (ver *Verifier) match_FcAtomInFactUnderUniFact_ConFc(fcAtomInFactUnderUniFa
 	// }
 
 	// 利用查prefix的方式来确定涉及到的param是不是 uni
-	if uniParamStr, ok := isUniParam(fcAtomInFactUnderUniFact); ok {
+	if uniParamStr, ok := ast.IsUniParam(fcAtomInFactUnderUniFact); ok {
 		retMap[uniParamStr] = []ast.Fc{conFc}
 		return retMap, true, nil
 	}
@@ -94,7 +96,8 @@ func (ver *Verifier) match_FcAtomInFactUnderUniFact_ConFc(fcAtomInFactUnderUniFa
 	return nil, false, nil
 }
 
-func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *ast.FcFn, conFc ast.Fc, uniParams []string) (map[string][]ast.Fc, bool, error) {
+// func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *ast.FcFn, conFc ast.Fc, uniParams []string) (map[string][]ast.Fc, bool, error) {
+func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *ast.FcFn, conFc ast.Fc) (map[string][]ast.Fc, bool, error) {
 	retMap := map[string][]ast.Fc{}
 
 	conParamAsFcFn, ok := conFc.(*ast.FcFn)
@@ -103,7 +106,8 @@ func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *
 	}
 
 	// match head
-	matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(fcFnUnFactUnderUniFact.FnHead, conParamAsFcFn.FnHead, uniParams)
+	// matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(fcFnUnFactUnderUniFact.FnHead, conParamAsFcFn.FnHead, uniParams)
+	matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(fcFnUnFactUnderUniFact.FnHead, conParamAsFcFn.FnHead)
 	if err != nil {
 		return nil, false, err
 	}
@@ -122,7 +126,8 @@ func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *
 		}
 
 		for j, param := range uniPipe {
-			matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(param, conParamAsFcFn.ParamSegs[i][j], uniParams)
+			// matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(param, conParamAsFcFn.ParamSegs[i][j], uniParams)
+			matchMap, ok, err := ver.match_FcInFactUnderUniFact_WithConFc(param, conParamAsFcFn.ParamSegs[i][j])
 			if err != nil {
 				return nil, false, err
 			}
@@ -146,13 +151,13 @@ func (ver *Verifier) match_FcFnInFactUnderUniFact_ConFc(fcFnUnFactUnderUniFact *
 // 	return "", false
 // }
 
-// 如果默认所有的 uniFact 的param都是以`开头的，而且里面的所有的事实都是以`开头，那这里根本不需要传入 uniParams，直接查看这个 fcAtom 的内容是否以`开头就行
-func isUniParam(fcAtomInFactUnderUniFact *ast.FcAtom) (string, bool) {
-	if strings.HasPrefix(fcAtomInFactUnderUniFact.Name, glob.UniParamPrefix) && fcAtomInFactUnderUniFact.PkgName == glob.BuiltinEmptyPkgName {
-		return fcAtomInFactUnderUniFact.Name, true
-	}
-	return "", false
-}
+// 如果默认所有的 uniFact 的param都是以`开头的(以一个`开头，或以n个`开头)，而且里面的所有的事实都是以`开头，那这里根本不需要传入 uniParams，直接查看这个 fcAtom 的内容是否以`开头就行
+// func isUniParam(fcAtomInFactUnderUniFact *ast.FcAtom) (string, bool) {
+// 	if strings.HasPrefix(fcAtomInFactUnderUniFact.Name, glob.UniParamPrefix) && fcAtomInFactUnderUniFact.PkgName == glob.BuiltinEmptyPkgName {
+// 		return fcAtomInFactUnderUniFact.Name, true
+// 	}
+// 	return "", false
+// }
 
 func mergeMatchMaps(from map[string][]ast.Fc, to map[string][]ast.Fc) {
 	for key, value := range from {
