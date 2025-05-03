18ef0e091b9fd17e99119e49d9ce3b7729e62dc4
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-03 11:17:26 +0800

    test

--- litex_executor/exe_test.go
+++ litex_executor/exe_test.go
@@ -616,7 +616,7 @@ func TestAllFactCode(t *testing.T) {
 	fmt.Printf("read file takes %v\nparsing takes %v\nexecution takes %v\n", readFileTime, parseTime, executionTime)
 }
 
-var code = readFile("../litex_code_examples/test_codes/matcher_env.lix")
+var code = readFile("../litex_code_examples/test_codes/prop.lix")
 
 func TestLastFactCode(t *testing.T) {
 	start := time.Now()
