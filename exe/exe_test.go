d0b9ec8763aeb052944c43ba16a9f7c6ba7a476e
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-10 14:01:57 +0800

    start to impl fnEqual

--- exe/exe_test.go
+++ exe/exe_test.go
@@ -606,7 +606,7 @@ func TestAllFactCode(t *testing.T) {
 	fmt.Printf("read file takes %v\nparsing takes %v\nexecution takes %v\n", readFileTime, parseTime, executionTime)
 }
 
-var code = readFile("../examples/test_codes/set_equal.lix")
+var code = readFile("../examples/test_codes/fn_equal.lix")
 
 func TestLastFactCode(t *testing.T) {
 	start := time.Now()
