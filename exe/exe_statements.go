12f252798187db3da7c1de917d89c197b71f96d2
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-15 15:50:48 +0800

    updating working hilbert geometry axioms. find bugs

--- exe/exe_statements.go
+++ exe/exe_statements.go
@@ -188,7 +188,7 @@ func (exec *Executor) defPropStmt(stmt *ast.DefPropStmt) error {
 
 	err = exec.env.NewFact(propToIff)
 
-	exec.appendMsg(fmt.Sprintf("%s\nknow by prop definition:\n%s", propToIff.String(), stmt.String()))
+	exec.appendMsg(fmt.Sprintf("%s\nis true prop definition:\n%s\n", propToIff.String(), stmt.String()))
 
 	if err != nil {
 		return err
@@ -199,7 +199,7 @@ func (exec *Executor) defPropStmt(stmt *ast.DefPropStmt) error {
 		return err
 	}
 
-	exec.appendMsg(fmt.Sprintf("%s\nknow by prop definition:\n%s", iffToProp.String(), stmt.String()))
+	exec.appendMsg(fmt.Sprintf("%s\nis true prop definition:\n%s\n", iffToProp.String(), stmt.String()))
 
 	if len(stmt.IffFacts) == 0 {
 		return nil
@@ -339,7 +339,7 @@ func (exec *Executor) haveStmt(stmt *ast.HaveStmt) (glob.ExecState, error) {
 		if err != nil {
 			return glob.ExecState_Error, err
 		}
-		exec.appendMsg(fmt.Sprintf("%s\nknow by %s definition:\n%s", domFact.String(), glob.KeywordExistProp, stmt.String()))
+		exec.appendMsg(fmt.Sprintf("%s\nis true %s definition:\n%s\n", domFact.String(), glob.KeywordExistProp, existPropDef.DefBody.DefHeader.Name))
 	}
 
 	// iff of def exist prop is true
@@ -348,7 +348,7 @@ func (exec *Executor) haveStmt(stmt *ast.HaveStmt) (glob.ExecState, error) {
 		if err != nil {
 			return glob.ExecState_Error, err
 		}
-		exec.appendMsg(fmt.Sprintf("%s\nknow by %s definition:\n%s", iffFact.String(), glob.KeywordExistProp, stmt.String()))
+		exec.appendMsg(fmt.Sprintf("%s\nis true %s definition:\n%s\n", iffFact.String(), glob.KeywordExistProp, existPropDef.DefBody.DefHeader.Name))
 	}
 
 	// 相关的 exist st 事实也成立
@@ -362,7 +362,7 @@ func (exec *Executor) haveStmt(stmt *ast.HaveStmt) (glob.ExecState, error) {
 	if err != nil {
 		return glob.ExecState_True, nil
 	}
-	exec.appendMsg(fmt.Sprintf("%s\nknow by %s definition:\n%s", newExistStFact.String(), glob.KeywordExistProp, stmt.String()))
+	exec.appendMsg(fmt.Sprintf("%s\nis true %s definition:\n%s\n", newExistStFact.String(), glob.KeywordExistProp, stmt.String()))
 
 	return glob.ExecState_True, nil
 }
@@ -659,7 +659,7 @@ func (exec *Executor) knowExistPropStmt(stmt *ast.KnowExistPropStmt) (glob.ExecS
 		return glob.ExecState_Error, err
 	}
 
-	exec.appendMsg(fmt.Sprintf("%s\nknow by %s definition:\n%s", knownUniFact.String(), stmt.ExistProp.DefBody.DefHeader.Name, stmt.String()))
+	exec.appendMsg(fmt.Sprintf("%s\nis true %s definition:\n%s\n", knownUniFact.String(), stmt.ExistProp.DefBody.DefHeader.Name, stmt.String()))
 
 	return glob.ExecState_True, nil
 }
