4b829cfedf58a5cd5229bc38379ce9626d5131d2
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-16 14:34:28 +0800

    new examples: compare with other formal languages

--- exe/exe_test_helpers.go
+++ exe/exe_test_helpers.go
@@ -71,7 +71,7 @@ func execStmtTest(topStmt []ast.TopStmt) []string {
 	if isNotTrue {
 		messages = append(messages, notTrueMessageBuilder.String())
 	} else {
-		messages = append(messages, "\nexecution success :)\n")
+		messages = append(messages, "---\nexecution success! :)")
 	}
 
 	return messages
