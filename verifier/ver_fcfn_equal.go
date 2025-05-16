0cf178e29f0f43adbf2a59994999d9239789278b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-16 09:10:55 +0800

    refacotr fcFn: now it has field []Fc instead of [][]Fc as parameters in ()

--- verifier/ver_fcfn_equal.go
+++ verifier/ver_fcfn_equal.go
@@ -13,124 +13,133 @@
 package litex_verifier
 
 import (
-	"fmt"
 	ast "golitex/ast"
+	glob "golitex/glob"
 )
 
 func (ver *Verifier) fcFnEq(left, right *ast.FcFn, state VerState) (bool, error) {
 	state = state.addRound()
 
-	for leftTailLen := 0; leftTailLen <= len(left.ParamSegs); leftTailLen++ {
-		ok, err := ver.fcFnHeadTailEq(left, right, state, leftTailLen)
-		if err != nil {
-			return false, err
+	if ok, err := ver.FactStmt(ast.NewSpecFactStmt(ast.TruePure, *ast.NewFcAtomWithName(glob.KeySymbolEqual), []ast.Fc{left, right}), state); err != nil {
+		if len(left.ParamSegs) != len(right.ParamSegs) {
+			return false, nil
 		}
-		if ok {
-			return true, nil
+
+		for i := range left.ParamSegs {
+			ok, err := ver.FactStmt(ast.NewSpecFactStmt(ast.TruePure, *ast.NewFcAtomWithName(glob.KeySymbolEqual), []ast.Fc{left.ParamSegs[i], right.ParamSegs[i]}), state)
+			if err != nil {
+				return false, err
+			}
+			if !ok {
+				return false, nil
+			}
 		}
+	} else if ok {
+		return true, nil
 	}
+
 	return false, nil
 }
 
-func (ver *Verifier) fcFnHeadTailEq(left, right *ast.FcFn, state VerState, leftTailLen int) (bool, error) {
-	if leftTailLen == 0 { // 必须存在，否则死循环
-		return ver.fcFnHeadEqLeftTailLenIs0(left, right, state)
-	}
+// func (ver *Verifier) fcFnHeadTailEq(left, right *ast.FcFn, state VerState, leftTailLen int) (bool, error) {
+// 	if leftTailLen == 0 { // 必须存在，否则死循环
+// 		return ver.fcFnHeadEqLeftTailLenIs0(left, right, state)
+// 	}
 
-	leftHeadLen := len(left.ParamSegs) - leftTailLen
-	rightHeadLen := len(right.ParamSegs) - leftTailLen
-	if rightHeadLen < 0 {
-		return false, nil
-	}
+// 	leftHeadLen := len(left.ParamSegs) - leftTailLen
+// 	rightHeadLen := len(right.ParamSegs) - leftTailLen
+// 	if rightHeadLen < 0 {
+// 		return false, nil
+// 	}
 
-	leftTails := left.ParamSegs[leftHeadLen:]
-	rightTails := right.ParamSegs[rightHeadLen:]
+// 	leftTails := left.ParamSegs[leftHeadLen:]
+// 	rightTails := right.ParamSegs[rightHeadLen:]
 
-	for i := range leftTailLen {
-		curLen := len(leftTails[i])
-		if curLen != len(rightTails[i]) {
-			return false, nil
-		}
+// 	for i := range leftTailLen {
+// 		curLen := len(leftTails[i])
+// 		if curLen != len(rightTails[i]) {
+// 			return false, nil
+// 		}
 
-		for j := range curLen {
-			// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(leftTails[i][j], rightTails[i][j])
-			ok, err := ver.fcEqualSpec(leftTails[i][j], rightTails[i][j], state)
-			// ok, err := ver.makeFcEqualFactAndVerify(leftTails[i][j], rightTails[i][j], curState)
-			if err != nil {
-				return false, err
-			}
-			if !ok {
-				return false, nil
-			}
-		}
-	}
+// 		for j := range curLen {
+// 			// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(leftTails[i][j], rightTails[i][j])
+// 			ok, err := ver.fcEqualSpec(leftTails[i][j], rightTails[i][j], state)
+// 			// ok, err := ver.makeFcEqualFactAndVerify(leftTails[i][j], rightTails[i][j], curState)
+// 			if err != nil {
+// 				return false, err
+// 			}
+// 			if !ok {
+// 				return false, nil
+// 			}
+// 		}
+// 	}
 
-	var leftHead ast.Fc
-	if leftHeadLen == 0 {
-		leftHead = left.FnHead
-	} else {
-		leftHead = &ast.FcFn{FnHead: left.FnHead, ParamSegs: left.ParamSegs[:leftHeadLen]}
-	}
+// 	var leftHead ast.Fc
+// 	if leftHeadLen == 0 {
+// 		leftHead = left.FnHead
+// 	} else {
+// 		leftHead = &ast.FcFn{FnHead: left.FnHead, ParamSegs: left.ParamSegs[:leftHeadLen]}
+// 	}
 
-	var rightHead ast.Fc
-	if rightHeadLen == 0 {
-		rightHead = right.FnHead
-	} else {
-		rightHead = &ast.FcFn{FnHead: right.FnHead, ParamSegs: right.ParamSegs[:rightHeadLen]}
-	}
+// 	var rightHead ast.Fc
+// 	if rightHeadLen == 0 {
+// 		rightHead = right.FnHead
+// 	} else {
+// 		rightHead = &ast.FcFn{FnHead: right.FnHead, ParamSegs: right.ParamSegs[:rightHeadLen]}
+// 	}
 
-	// ok, err := ver.makeFcEqualFactAndVerify(leftHead, rightHead, curState)
-	// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(leftHead, rightHead)
-	ok, err := ver.fcEqualSpec(leftHead, rightHead, state)
-	if err != nil {
-		return false, err
-	}
-	if !ok {
-		return false, nil
-	}
+// 	// ok, err := ver.makeFcEqualFactAndVerify(leftHead, rightHead, curState)
+// 	// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(leftHead, rightHead)
+// 	ok, err := ver.fcEqualSpec(leftHead, rightHead, state)
+// 	if err != nil {
+// 		return false, err
+// 	}
+// 	if !ok {
+// 		return false, nil
+// 	}
 
-	if state.requireMsg() {
-		ver.successMsgEnd(fmt.Sprintf("%s = %s", left.String(), right.String()), fmt.Sprintf("%s = %s\n%s = %s", leftHead.String(), rightHead.String(), left.String(), right.String()))
-	}
+// 	if state.requireMsg() {
+// 		ver.successMsgEnd(fmt.Sprintf("%s = %s", left.String(), right.String()), fmt.Sprintf("%s = %s\n%s = %s", leftHead.String(), rightHead.String(), left.String(), right.String()))
+// 	}
 
-	return true, nil
-}
+// 	return true, nil
+// }
 
-func (ver *Verifier) fcFnHeadEqLeftTailLenIs0(left, right *ast.FcFn, state VerState) (bool, error) {
-	// ? 这里应该是fcEqualSpec还是 fcCmpLiterally??
+// func (ver *Verifier) fcFnHeadEqLeftTailLenIs0(left, right *ast.FcFn, state VerState) (bool, error) {
+// 	// ? 这里应该是fcEqualSpec还是 fcCmpLiterally??
 
-	if len(left.ParamSegs) != len(right.ParamSegs) {
-		return false, nil
-	}
+// 	if len(left.ParamSegs) != len(right.ParamSegs) {
+// 		return false, nil
+// 	}
 
-	// ok, err := ver.makeFcEqualFactAndVerify(left.FnHead, right.FnHead, state)
-	// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(left.FnHead, right.FnHead)
-	ok, err := ver.fcEqualSpec(left.FnHead, right.FnHead, state)
-	if err != nil {
-		return false, err
-	}
-	if !ok {
-		return false, nil
-	}
+// 	// ok, err := ver.makeFcEqualFactAndVerify(left.FnHead, right.FnHead, state)
+// 	// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(left.FnHead, right.FnHead)
+// 	ok, err := ver.fcEqualSpec(left.FnHead, right.FnHead, state)
+// 	if err != nil {
+// 		return false, err
+// 	}
+// 	if !ok {
+// 		return false, nil
+// 	}
 
-	for i := range left.ParamSegs {
-		if len(left.ParamSegs[i]) != len(right.ParamSegs[i]) {
-			return false, nil
-		}
+// 	for i := range left.ParamSegs {
+// 		if len(left.ParamSegs[i]) != len(right.ParamSegs[i]) {
+// 			return false, nil
+// 		}
 
-		for j := range left.ParamSegs[i] {
-			// ok, err := ver.makeFcEqualFactAndVerify(left.ParamSegs[i][j], right.ParamSegs[i][j], curState)
-			// 这里相当于强行只能用 specFact 来证明，不能用forall来证；如果是用forall的话，就需要 makeFcEqualFactAndVerify
-			// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(left.ParamSegs[i][j], right.ParamSegs[i][j])
-			ok, err := ver.fcEqualSpec(left.ParamSegs[i][j], right.ParamSegs[i][j], state)
-			if err != nil {
-				return false, err
-			}
-			if !ok {
-				return false, nil
-			}
-		}
-	}
+// 		for j := range left.ParamSegs[i] {
+// 			// ok, err := ver.makeFcEqualFactAndVerify(left.ParamSegs[i][j], right.ParamSegs[i][j], curState)
+// 			// 这里相当于强行只能用 specFact 来证明，不能用forall来证；如果是用forall的话，就需要 makeFcEqualFactAndVerify
+// 			// ok, err := ver.iterateOverKnownSpecEqualFactsAndCheck(left.ParamSegs[i][j], right.ParamSegs[i][j])
+// 			ok, err := ver.fcEqualSpec(left.ParamSegs[i][j], right.ParamSegs[i][j], state)
+// 			if err != nil {
+// 				return false, err
+// 			}
+// 			if !ok {
+// 				return false, nil
+// 			}
+// 		}
+// 	}
 
-	return true, nil
-}
+// 	return true, nil
+// }
