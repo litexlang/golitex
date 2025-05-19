dc0b0becd45e44a3e879f030975092b244bab68f
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-19 12:52:04 +0800

    order number expr +-*/

--- ast/ast_tmp.go
+++ ast/ast_tmp.go
@@ -1,52 +1,158 @@
 package litex_ast
 
-func (stmt *FcFn) isAdd() bool {
+import "fmt"
+
+func (stmt *FcFn) isAdd() (bool, error) {
 	asAtom, ok := stmt.FnHead.(*FcAtom)
 	if !ok {
-		return false
+		return false, nil
 	}
-	return asAtom.Name == "+"
+	if asAtom.Name == "+" {
+		if len(stmt.ParamSegs) != 2 {
+			return false, fmt.Errorf("add must have 2 params, but got %d", len(stmt.ParamSegs))
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
-func (stmt *FcFn) isMul() bool {
+func (stmt *FcFn) isMul() (bool, error) {
 	asAtom, ok := stmt.FnHead.(*FcAtom)
 	if !ok {
-		return false
+		return false, nil
 	}
-	return asAtom.Name == "*"
+	if asAtom.Name == "*" {
+		if len(stmt.ParamSegs) != 2 {
+			return false, fmt.Errorf("mul must have 2 params, but got %d", len(stmt.ParamSegs))
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
-func (stmt *FcFn) isSub() bool {
+func (stmt *FcFn) isMinusAsInfix() (bool, error) {
 	asAtom, ok := stmt.FnHead.(*FcAtom)
 	if !ok {
-		return false
+		return false, nil
 	}
-	return asAtom.Name == "-"
+	if asAtom.Name == "-" {
+		if len(stmt.ParamSegs) != 2 {
+			return false, fmt.Errorf("sub must have 2 params, but got %d", len(stmt.ParamSegs))
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
-func (stmt *FcFn) isDiv() bool {
+func (stmt *FcFn) isMinusAsPrefix() (bool, error) {
 	asAtom, ok := stmt.FnHead.(*FcAtom)
 	if !ok {
-		return false
+		return false, nil
 	}
-	return asAtom.Name == "/"
+	if asAtom.Name == "-" {
+		if len(stmt.ParamSegs) != 1 {
+			return false, fmt.Errorf("minus must have 1 param, but got %d", len(stmt.ParamSegs))
+		}
+		return true, nil
+	}
+	return false, nil
 }
 
-func (stmt *FcAtom) orderAddition() []Fc {
-	return []Fc{stmt}
-}
-
-func (stmt *FcFn) orderAddition() []Fc {
+func (stmt *FcFn) isDiv() (bool, error) {
 	asAtom, ok := stmt.FnHead.(*FcAtom)
 	if !ok {
-		return nil
+		return false, nil
 	}
-	orderedLeft := stmt.ParamSegs[0].orderAddition()
-	orderedRight := stmt.ParamSegs[1].orderAddition()
+	if asAtom.Name == "/" {
+		if len(stmt.ParamSegs) != 2 {
+			return false, fmt.Errorf("div must have 2 params, but got %d", len(stmt.ParamSegs))
+		}
+		return true, nil
+	}
+	return false, nil
+}
 
-	orderedLeft = append(orderedLeft, asAtom)
-	orderedRight = append(orderedRight, asAtom)
+func (stmt *FcAtom) orderNumberExpr() ([]Fc, error) {
+	return []Fc{stmt}, nil
+}
 
-	// TODO
+func (stmt *FcFn) orderNumberExpr() ([]Fc, error) {
+	isAdd, err := stmt.isAdd()
+	if err != nil {
+		return nil, err
+	}
+	if isAdd {
+		return stmt.orderAddFcFn()
+	}
+
+	isMul, err := stmt.isMul()
+	if err != nil {
+		return nil, err
+	}
+	if isMul {
+		return stmt.orderMulFcFn()
+	}
+
+	isMinusAsInfix, err := stmt.isMinusAsInfix()
+	if err != nil {
+		return nil, err
+	}
+	if isMinusAsInfix {
+		return stmt.orderMinusAsInfixFcFn()
+	}
+
+	isMinusAsPrefix, err := stmt.isMinusAsPrefix()
+	if err != nil {
+		return nil, err
+	}
+	if isMinusAsPrefix {
+		return stmt.orderMinusAsPrefixFcFn()
+	}
+
+	isDiv, err := stmt.isDiv()
+	if err != nil {
+		return nil, err
+	}
+	if isDiv {
+		return stmt.orderDivFcFn()
+	}
+
+	return nil, nil
+}
+
+func (stmt *FcFn) orderAddFcFn() ([]Fc, error) {
+	orderedLeft, err := stmt.ParamSegs[0].orderNumberExpr()
+	if err != nil {
+		return nil, err
+	}
+	orderedRight, err := stmt.ParamSegs[1].orderNumberExpr()
+	if err != nil {
+		return nil, err
+	}
+
+	orderedFc := mergeOrderedAdditionNumberExpr(orderedLeft, orderedRight)
+
+	return orderedFc, nil
+}
+
+func mergeOrderedAdditionNumberExpr(orderedLeft []Fc, orderedRight []Fc) []Fc {
+	_ = orderedLeft
+	_ = orderedRight
 	return []Fc{}
+}
+
+func (stmt *FcFn) orderMulFcFn() ([]Fc, error) {
+	return []Fc{}, nil
+}
+
+func (stmt *FcFn) orderMinusAsInfixFcFn() ([]Fc, error) {
+	return []Fc{}, nil
+}
+
+func (stmt *FcFn) orderDivFcFn() ([]Fc, error) {
+	return []Fc{}, nil
+}
+
+func (stmt *FcFn) orderMinusAsPrefixFcFn() ([]Fc, error) {
+	return []Fc{}, nil
 }
