190718cd801a17d2bfe0f0022124e2563b497978
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-20 17:34:24 +0800

    main

--- main.go
+++ main.go
@@ -13,15 +13,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	sys "golitex/sys"
+	"os"
 )
 
 func main() {
-	msg, signal, err := sys.RunFile("./examples/comprehensive_examples/syllogism(三段论).lix")
-	if err != nil {
-		fmt.Println(err.Error())
+	// Define flags
+	helpFlag := flag.Bool("help", false, "Show help message")
+	versionFlag := flag.Bool("version", false, "Show version information")
+	defaultFile := "./examples/comprehensive_examples/syllogism(三段论).lix"
+
+	flag.Parse()
+
+	// Handle flags
+	if *helpFlag {
+		fmt.Println("Usage: program [options] [filename]")
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		fmt.Printf("\nIf no filename provided, defaults to: %s\n", defaultFile)
+		return
 	}
-	fmt.Println(sys.BetterMsg(msg))
-	fmt.Println(signal)
+
+	if *versionFlag {
+		fmt.Println("Litex Processor version -1")
+		return
+	}
+
+	// Get non-flag arguments
+	args := flag.Args()
+	var filePath string
+
+	if len(args) == 0 {
+		filePath = defaultFile
+	} else {
+		filePath = args[0]
+	}
+
+	// Verify file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		fmt.Printf("Error: File '%s' does not exist\n", filePath)
+		os.Exit(1)
+	}
+
+	// Process file
+	msg, signal, err := sys.RunFile(filePath)
+	if err != nil {
+		fmt.Printf("Processing error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Output results
+	fmt.Println("Output:", sys.BetterMsg(msg))
+	fmt.Println("Status:", signal)
 }
