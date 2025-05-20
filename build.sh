970a40a1b3dda60a7257acbbe9453a6bf38ca3ca
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-20 17:18:49 +0800

    binary, build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-GOOS=windows GOARCH=amd64 go build -o ./binary/litex_windows.exe main.go
+GOOS=windows GOARCH=amd64 go build -o ./binary/windows_litex.exe main.go
 
-GOOS=linux GOARCH=amd64 go build -o ./binary/litex_linux main.go
+GOOS=linux GOARCH=amd64 go build -o ./binary/linux_litex main.go
 
-GOOS=darwin GOARCH=amd64 go build -o ./binary/litex_mac main.go
+GOOS=darwin GOARCH=amd64 go build -o ./binary/mac_litex main.go
