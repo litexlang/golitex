00576e96ba9ed7f767fad4f0eabd7a2d7902c05b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-21 16:02:31 +0800

    20k lines

--- number/num_arithmetic.go
+++ number/num_arithmetic.go
@@ -1,3 +1,15 @@
+// Copyright 2024 Jiachen Shen.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Original Author: Jiachen Shen <malloc_realloc_free@outlook.com>
+// Contact the development team: <litexlang@outlook.com>
+// Visit litexlang.org and https://github.com/litexlang/golitex for more info.
+
 package litex_num
 
 import (
@@ -235,7 +247,7 @@ func simplify(poly Polynomial) Polynomial {
 
 // --- High-level entry point ---
 
-func parseAndExpand(expr string) Polynomial {
+func ParseAndExpand(expr string) Polynomial {
 	tokens := tokenize(expr)
 	ast := parseExpr(tokens)
 	poly := eval(ast)
