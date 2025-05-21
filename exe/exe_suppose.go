aa7602806f9dd2fecd66399a2537d2666d4c2226
malloc-realloc <malloc_realloc_free@outlook.com>
2025-05-21 10:23:45 +0800

    clean

--- exe/exe_suppose.go
+++ exe/exe_suppose.go
@@ -1,3 +1,15 @@
+// Copyright 2024 Jiachen Shen.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Original Author: Jiachen Shen <malloc_realloc_free@outlook.com>
+// Contact the development team: <litexlang@outlook.com>
+// Visit litexlang.org and https://github.com/litexlang/golitex for more info.
+
 package litex_executor
 
 import (
@@ -18,6 +30,8 @@ func (exec *Executor) supposePropMatchStmt(stmt *ast.SupposePropMatchStmt) (glob
 
 	exec.newEnv(originalEnv, stmt)
 	defer exec.deleteEnvAndRetainMsg()
+
+	// declare new params in suppose environment
 
 	factSpecDef, ok := exec.env.GetPropDef(stmt.Fact.PropName)
 	if !ok {
