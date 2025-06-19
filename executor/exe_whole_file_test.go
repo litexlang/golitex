02d59e0d4d7f73ca7448f24fcd03624be3f95601
malloc-realloc <malloc_realloc_free@outlook.com>
2025-06-19 15:02:51 +0800

    example: fn_leads_to_exist.lix

--- executor/exe_whole_file_test.go
+++ executor/exe_whole_file_test.go
@@ -23,7 +23,7 @@ import (
 func TestWholeFile(t *testing.T) {
 	start := time.Now()
 	// codePath := "../examples/test_codes/builtin_cmp.lix"
-	codePath := "../examples/test_codes/claim.lix"
+	codePath := "../examples/test_codes/fn_leads_to_exist.lix"
 	readFileTime := time.Since(start)
 	start = time.Now()
 	topStmtSlice, err := setupAndParseStmtTest(codePath)
