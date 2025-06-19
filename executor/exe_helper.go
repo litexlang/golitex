fc1d5514e6670ff06626d94101cbd043abf5c043
malloc-realloc <malloc_realloc_free@outlook.com>
2025-06-19 09:18:42 +0800

    update: prove by contradiction

---
+++ executor/exe_helper.go
@@ -0,0 +1,27 @@
+// Copyright 2024 Jiachen Shen.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Original Author: Jiachen Shen <malloc_realloc_free@outlook.com>
+// Litex email: <litexlang@outlook.com>
+// Litex website: https://litexlang.org
+// Litex github repository: https://github.com/litexlang/golitex
+// Litex Zulip community: https://litex.zulipchat.com
+
+package litex_executor
+
+import glob "golitex/glob"
+
+func notOkExec(state glob.ExecState, err error) bool {
+	if err != nil {
+		return true
+	}
+	if state != glob.ExecState_True {
+		return true
+	}
+	return false
+}
