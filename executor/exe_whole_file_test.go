d96d4fbbd614cdaa1ff9ab398908c0cd5cce3e5b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-06-23 20:21:25 +0800

    clean

--- executor/exe_whole_file_test.go
+++ executor/exe_whole_file_test.go
@@ -23,7 +23,7 @@ import (
 func TestWholeFile(t *testing.T) {
 	start := time.Now()
 	// codePath := "../examples/test_codes/builtin_cmp.lix"
-	codePath := "../examples/test_codes/fn_template1.lix"
+	codePath := "../examples/test_codes/fn_template2.lix"
 	readFileTime := time.Since(start)
 	start = time.Now()
 	topStmtSlice, err := setupAndParseStmtTest(codePath)
