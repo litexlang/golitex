bcd37df3994747074fb22bed78079e33d2faa1a8
malloc-realloc <malloc_realloc_free@outlook.com>
2025-07-01 16:03:09 +0800

    give up for the time being: have by replacement. situation is not mature enough

--- executor/exe_have_by_replacement.go
+++ executor/exe_have_by_replacement.go
@@ -15,58 +15,58 @@
 package litex_executor
 
 import (
-	"fmt"
 	ast "golitex/ast"
 	glob "golitex/glob"
-	verifier "golitex/verifier"
 )
 
 func (exec *Executor) haveByReplacementStmt(stmt *ast.HaveByReplacementStmt) (glob.ExecState, error) {
-	exec.newEnv(exec.env, exec.env.CurMatchProp)
-	defer exec.deleteEnvAndRetainMsg()
+	panic("not implemented")
+	// exec.newEnv(exec.env, exec.env.CurMatchProp)
+	// defer exec.deleteEnvAndRetainMsg()
 
-	ver := verifier.NewVerifier(exec.env)
+	// ver := verifier.NewVerifier(exec.env)
 
-	// domSet and rangeSet are sets
-	ok, err := ver.VerFactStmt(ast.NewInFactWithParamFc(stmt.DomSet, ast.FcAtom(glob.KeywordSet)), verifier.Round0NoMsg)
-	if err != nil {
-		return glob.ExecState_Error, fmt.Errorf("error verifying domSet and rangeSet: %v", err)
-	}
+	// // domSet and rangeSet are sets
+	// ok, err := ver.VerFactStmt(ast.NewInFactWithParamFc(stmt.DomSet, ast.FcAtom(glob.KeywordSet)), verifier.Round0NoMsg)
+	// if err != nil {
+	// 	return glob.ExecState_Error, fmt.Errorf("error verifying domSet and rangeSet: %v", err)
+	// }
 
-	if !ok {
-		return glob.ExecState_Error, fmt.Errorf("domSet and rangeSet are not sets")
-	}
+	// if !ok {
+	// 	return glob.ExecState_Error, fmt.Errorf("domSet and rangeSet are not sets")
+	// }
 
-	ok, err = ver.VerFactStmt(ast.NewInFactWithParamFc(stmt.RangeSet, ast.FcAtom(glob.KeywordSet)), verifier.Round0NoMsg)
-	if err != nil {
-		return glob.ExecState_Error, fmt.Errorf("error verifying rangeSet: %v", err)
-	}
+	// ok, err = ver.VerFactStmt(ast.NewInFactWithParamFc(stmt.RangeSet, ast.FcAtom(glob.KeywordSet)), verifier.Round0NoMsg)
+	// if err != nil {
+	// 	return glob.ExecState_Error, fmt.Errorf("error verifying rangeSet: %v", err)
+	// }
 
-	if !ok {
-		return glob.ExecState_Error, fmt.Errorf("rangeSet is not a set")
-	}
+	// if !ok {
+	// 	return glob.ExecState_Error, fmt.Errorf("rangeSet is not a set")
+	// }
 
-	params := []string{"x", "y1", "y2"}
-	paramSets := []ast.Fc{stmt.DomSet, stmt.RangeSet, stmt.RangeSet}
-	domFacts := []ast.FactStmt{
-		ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(stmt.PropName), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y1")}),
-		ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(stmt.PropName), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y2")}),
-	}
-	thenFacts := []ast.FactStmt{
-		ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(glob.KeywordLastTwoObjectsAreEqual), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y1"), ast.FcAtom("y2")}),
-	}
+	// params := []string{"x", "y1", "y2"}
+	// paramSets := []ast.Fc{stmt.DomSet, stmt.RangeSet, stmt.RangeSet}
+	// domFacts := []ast.FactStmt{
+	// 	ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(stmt.PropName), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y1")}),
+	// 	ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(stmt.PropName), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y2")}),
+	// }
+	// thenFacts := []ast.FactStmt{
+	// 	ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(glob.KeywordLastTwoObjectsAreEqual), []ast.Fc{ast.FcAtom("x"), ast.FcAtom("y1"), ast.FcAtom("y2")}),
+	// }
 
-	uniFact := ast.NewUniFact(params, paramSets, domFacts, thenFacts)
+	// uniFact := ast.NewUniFact(params, paramSets, domFacts, thenFacts)
 
-	ok, err = ver.VerFactStmt(uniFact, verifier.Round0NoMsg)
-	if err != nil {
-		return glob.ExecState_Error, fmt.Errorf("error verifying have by replacement: %v", err)
-	}
+	// ok, err = ver.VerFactStmt(uniFact, verifier.Round0NoMsg)
+	// if err != nil {
+	// 	return glob.ExecState_Error, fmt.Errorf("error verifying have by replacement: %v", err)
+	// }
 
-	if !ok {
-		return glob.ExecState_Error, fmt.Errorf("have by replacement is not true")
-	}
+	// if !ok {
+	// 	return glob.ExecState_Error, fmt.Errorf("have by replacement is not true")
+	// }
 
 	// properties of introduced symbol
-	return glob.ExecState_True, nil
+	// forall y in rangeSet, exist x st x $propName y
+
 }
