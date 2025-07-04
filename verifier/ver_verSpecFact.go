d13b5d6417ae2248f2c4685b6f87bd4ea0923663
malloc-realloc <malloc_realloc_free@outlook.com>
2025-07-04 11:49:30 +0800

    checkpoint: implement: exist xxx st exist_in(setName), have xxx st exist_in(etName) to make sure we can get something from a non empty set

--- verifier/ver_verSpecFact.go
+++ verifier/ver_verSpecFact.go
@@ -121,6 +121,8 @@ func (ver *Verifier) verSpecFactStepByStep(stmt *ast.SpecFactStmt, state VerStat
 func (ver *Verifier) verSpecialSpecFact_ByBIR(stmt *ast.SpecFactStmt, state VerState) (bool, error) {
 	if stmt.NameIs(glob.KeywordIn) {
 		return ver.inFactBuiltinRules(stmt, state)
+	} else if stmt.NameIs(glob.KeywordExistIn) && stmt.TypeEnum == ast.TrueExist_St {
+		return ver.trueExistInSt(stmt, state)
 	}
 
 	if ok, err := ver.verNumberLogicRelaOpt_BuiltinRules(stmt, state); err != nil {
