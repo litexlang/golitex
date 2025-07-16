c4819d56ab032b4586f729979cefe54480008ad9
malloc-realloc <malloc_realloc_free@outlook.com>
2025-07-16 10:42:26 +0800

    clean

--- environment/env_get_known_facts.go
+++ environment/env_get_known_facts.go
@@ -49,16 +49,21 @@ func (e *Env) GetTemplateOfFcFnRecursively(fcFn *ast.FcFn) ([]*ast.FnTemplateStm
 
 	// 从 template 的定义中，得到 template的返回值类型
 	for i := count - 1; i >= 0; i-- {
-		retSetAtAtom, ok := fnDefRetSet.(ast.FcAtom)
-		if !ok {
-			return nil, nil, false
+		switch retSet := fnDefRetSet.(type) {
+		case ast.FcAtom:
+			retSet, ok := fnDefRetSet.(ast.FcAtom)
+			if !ok {
+				return nil, nil, false
+			}
+			templateDef, ok := e.GetFnTemplateDef(retSet)
+			if !ok {
+				return nil, nil, false
+			}
+			templateOfEachLevel = append(templateOfEachLevel, &templateDef.FnTemplateStmt)
+			fnDefRetSet = templateDef.FnTemplateStmt.RetSet
+		case *ast.FcFn:
+			panic("")
 		}
-		templateDef, ok := e.GetFnTemplateDef(retSetAtAtom)
-		if !ok {
-			return nil, nil, false
-		}
-		templateOfEachLevel = append(templateOfEachLevel, &templateDef.FnTemplateStmt)
-		fnDefRetSet = templateDef.FnTemplateStmt.RetSet
 	}
 
 	return templateOfEachLevel, fcOfEachLevel, true
