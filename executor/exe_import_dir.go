120efddd0300ad8334ad621f85d4e07570402893
malloc-realloc <malloc_realloc_free@outlook.com>
2025-08-04 19:27:11 +0800

    imo 2024 short list a5 solution2

--- executor/exe_import_dir.go
+++ executor/exe_import_dir.go
@@ -143,9 +143,6 @@ func getGloballyImportedStmtSlice(code string) ([]ast.Stmt, error) {
 
 	ret := []ast.Stmt{}
 	for _, topStmt := range topStmtSlice {
-		// if _, ok := topStmt.(*ast.ImportGloballyStmt); ok {
-		// 	continue
-		// } else
 		if _, ok := topStmt.(*ast.ImportDirStmt); ok {
 			continue
 		} else {
