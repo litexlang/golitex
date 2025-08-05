5fff002b938ec977a0ed9fe7f9bffbc8902e66e0
malloc-realloc <malloc_realloc_free@outlook.com>
2025-08-05 12:14:28 +0800

    checkpoint: inline fact; check uni fact layer in parser time

--- parser/parser_uniFact_in_uniFact_checker.go
+++ parser/parser_uniFact_in_uniFact_checker.go
@@ -19,19 +19,10 @@ import (
 	ast "golitex/ast"
 )
 
-func checkUniFactDepth0(uniFact *ast.UniFactStmt) error {
-	for _, fact := range uniFact.DomFacts {
+func checkFactsUniDepth(facts []ast.FactStmt) error {
+	for _, fact := range facts {
 		if asUniFact, ok := fact.(*ast.UniFactStmt); ok {
-			err := checkUniFactDepth1(asUniFact)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	for _, fact := range uniFact.ThenFacts {
-		if asUniFact, ok := fact.(*ast.UniFactStmt); ok {
-			err := checkUniFactDepth1(asUniFact)
+			err := checkUniFactDepth0(asUniFact)
 			if err != nil {
 				return err
 			}
@@ -41,18 +32,38 @@ func checkUniFactDepth0(uniFact *ast.UniFactStmt) error {
 	return nil
 }
 
-func checkUniFactDepth1(uniFact *ast.UniFactStmt) error {
+func checkUniFactDepth0(uniFact *ast.UniFactStmt) error {
+	for _, fact := range uniFact.DomFacts {
+		if asUniFact, ok := fact.(*ast.UniFactStmt); ok {
+			if !checkUniFactDepth1(asUniFact) {
+				return fmt.Errorf("too many levels of universal fact in universal fact:\n%s\nthere must be at most two levels of universal fact", uniFact.String())
+			}
+		}
+	}
+
+	for _, fact := range uniFact.ThenFacts {
+		if asUniFact, ok := fact.(*ast.UniFactStmt); ok {
+			if !checkUniFactDepth1(asUniFact) {
+				return fmt.Errorf("too many levels of universal fact in universal fact:\n%s\nthere must be at most two levels of universal fact", uniFact.String())
+			}
+		}
+	}
+
+	return nil
+}
+
+func checkUniFactDepth1(uniFact *ast.UniFactStmt) bool {
 	for _, fact := range uniFact.DomFacts {
 		if _, ok := fact.(*ast.UniFactStmt); ok {
-			return fmt.Errorf("dom fact cannot be a uni fact")
+			return false
 		}
 	}
 
 	for _, fact := range uniFact.ThenFacts {
 		if _, ok := fact.(*ast.UniFactStmt); ok {
-			return fmt.Errorf("then fact cannot be a uni fact")
+			return false
 		}
 	}
 
-	return nil
+	return true
 }
