f720948720bfc4f45bc1189e8dc93db143e9c044
malloc-realloc <malloc_realloc_free@outlook.com>
2025-08-13 16:30:48 +0800

    fix bugs

--- executor/exe_prove_by_induction.go
+++ executor/exe_prove_by_induction.go
@@ -44,7 +44,7 @@ func (exec *Executor) proveByInductionStmt(stmt *ast.ProveByInductionStmt) (glob
 	}()
 
 	// 输入的 Start 必须是 N_pos
-	startIsNPos := exec.proveByInduction_Fact_Start_is_NPos(stmt)
+	startIsNPos := proveByInduction_Fact_Start_is_NPos(stmt)
 	ok, err := ver.VerFactStmt(startIsNPos, verifier.Round0NoMsg)
 	if err != nil {
 		return glob.ExecState_Error, err
@@ -55,7 +55,7 @@ func (exec *Executor) proveByInductionStmt(stmt *ast.ProveByInductionStmt) (glob
 	}
 
 	// 把start代入fact，得到的fact是true
-	startFact, err := exec.proveByInduction_newStartFact(stmt)
+	startFact, err := proveByInduction_newStartFact(stmt)
 	if err != nil {
 		return glob.ExecState_Error, err
 	}
@@ -69,7 +69,7 @@ func (exec *Executor) proveByInductionStmt(stmt *ast.ProveByInductionStmt) (glob
 	}
 
 	// 对于任意n对于fact成立，那么对于n+1也成立
-	uniFact_n_true_leads_n_plus_1_true, err := exec.proveByInduction_newUniFact_n_true_leads_n_plus_1_true(stmt)
+	uniFact_n_true_leads_n_plus_1_true, err := proveByInduction_newUniFact_n_true_leads_n_plus_1_true(stmt)
 	if err != nil {
 		return glob.ExecState_Error, err
 	}
@@ -82,22 +82,28 @@ func (exec *Executor) proveByInductionStmt(stmt *ast.ProveByInductionStmt) (glob
 		return glob.ExecState_Unknown, nil
 	}
 
-	isOk = true
+	// 对于任何 param >= start, fact 成立
+	uniFact_forall_param_geq_start_then_fact_is_true := proveByInduction_newUniFact_forall_param_geq_start_then_fact_is_true(stmt)
+	err = exec.env.NewFact(uniFact_forall_param_geq_start_then_fact_is_true)
+	if err != nil {
+		return glob.ExecState_Error, err
+	}
 
+	isOk = true
 	return glob.ExecState_True, nil
 }
 
-func (exec *Executor) proveByInduction_Fact_Start_is_NPos(stmt *ast.ProveByInductionStmt) *ast.SpecFactStmt {
+func proveByInduction_Fact_Start_is_NPos(stmt *ast.ProveByInductionStmt) *ast.SpecFactStmt {
 	startIsNPos := ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(glob.KeywordIn), []ast.Fc{stmt.Start, ast.FcAtom(glob.KeywordNPos)})
 	return startIsNPos
 }
 
-func (exec *Executor) proveByInduction_newStartFact(stmt *ast.ProveByInductionStmt) (ast.FactStmt, error) {
+func proveByInduction_newStartFact(stmt *ast.ProveByInductionStmt) (ast.FactStmt, error) {
 	startFact, err := stmt.Fact.Instantiate(map[string]ast.Fc{stmt.Param: stmt.Start})
 	return startFact, err
 }
 
-func (exec *Executor) proveByInduction_newUniFact_n_true_leads_n_plus_1_true(stmt *ast.ProveByInductionStmt) (ast.FactStmt, error) {
+func proveByInduction_newUniFact_n_true_leads_n_plus_1_true(stmt *ast.ProveByInductionStmt) (ast.FactStmt, error) {
 	uniMap := map[string]ast.Fc{stmt.Param: ast.NewFcFn(ast.FcAtom(glob.KeySymbolPlus), []ast.Fc{ast.FcAtom(stmt.Param), ast.FcAtom("1")})}
 
 	retUniFactDom := []ast.FactStmt{
@@ -111,4 +117,8 @@ func (exec *Executor) proveByInduction_newUniFact_n_true_leads_n_plus_1_true(stm
 	}
 
 	return ast.NewUniFact([]string{stmt.Param}, []ast.Fc{ast.FcAtom(glob.KeywordNPos)}, retUniFactDom, []ast.FactStmt{retUniFactThen}), nil
+}
+
+func proveByInduction_newUniFact_forall_param_geq_start_then_fact_is_true(stmt *ast.ProveByInductionStmt) ast.FactStmt {
+	return ast.NewUniFact([]string{stmt.Param}, []ast.Fc{ast.FcAtom(glob.KeywordNPos)}, []ast.FactStmt{ast.NewSpecFactStmt(ast.TruePure, ast.FcAtom(glob.KeySymbolLargerEqual), []ast.Fc{ast.FcAtom(stmt.Param), stmt.Start})}, []ast.FactStmt{stmt.Fact})
 }
