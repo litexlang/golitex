2ed65778c665c9f21e6edf82bac980412c347e28
malloc-realloc <malloc_realloc_free@outlook.com>
2025-08-25 14:58:24 +0800

    replace fc

---
+++ ast/ast_replace_fc.go
@@ -0,0 +1,43 @@
+// Copyright 2024 Jiachen Shen.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Original Author: Jiachen Shen <malloc_realloc_free@outlook.com>
+// Litex email: <litexlang@outlook.com>
+// Litex website: https://litexlang.org
+// Litex github repository: https://github.com/litexlang/golitex
+// Litex Zulip community: https://litex.zulipchat.com/join/c4e7foogy6paz2sghjnbujov/
+
+package litex_ast
+
+func (stmt *SpecFactStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *UniFactStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *UniFactWithIffStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *OrStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *EnumStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *IntensionalSetStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
+
+func (stmt *EqualsFactStmt) ReplaceFc(oldFc Fc, newFc Fc) FactStmt {
+	panic("")
+}
