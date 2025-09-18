a0fe086b8fcd75bf1529f2a90087771caf9b97c0
malloc-realloc <malloc_realloc_free@outlook.com>
2025-09-18 09:23:08 +0800

    data cleaner

--- data_cleaner/data_cleaner_test.go
+++ data_cleaner/data_cleaner_test.go
@@ -15,7 +15,9 @@
 package data_cleaner
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -63,5 +65,50 @@ prove:
 		fmt.Println("Proofs:")
 		fmt.Println(cleanData.ClaimData.Proofs)
 		fmt.Println("--------------------------------")
+	}
+}
+
+func TestRead_CSV_OutputCleanData(t *testing.T) {
+	fileSlices := []string{
+		// "../past_examples/minif2f.csv",
+		// "../past_examples/basic_math.csv",
+		"../past_examples/basic_math2.csv",
+		// "../past_examples/gsm.csv",
+	}
+
+	for _, filePath := range fileSlices {
+		// 把filePath的文件名改成cleaned_data.csv
+		store_to := filePath[:len(filePath)-4] + "_cleaned_data.csv"
+		store_file, err := os.Create(store_to)
+		if err != nil {
+			t.Errorf("failed to create file %s\n", store_to)
+		}
+		defer store_file.Close()
+		file, err := os.Open(filePath)
+		if err != nil {
+			t.Errorf("failed to open file %s\n", filePath)
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		records, err := reader.ReadAll()
+		if err != nil {
+			t.Errorf("failed to read csv file %s\n", filePath)
+		}
+
+		for _, record := range records {
+			code := record[2]
+			cleanDataSlice, err := CleanStmtSlice(code)
+			if err != nil {
+				t.Errorf("failed to clean data %s\n", code)
+			}
+			cleanData := cleanDataSlice[len(cleanDataSlice)-1]
+
+			// 把这一行写入：record[0], record[1], record[2], cleanData.Assumptions, cleanData.ClaimData.ClaimAssumption, cleanData.ClaimData.ClaimResult, cleanData.ClaimData.Proofs
+
+			writer := csv.NewWriter(store_file)
+			writer.Write([]string{record[0], record[1], record[2], cleanData.Assumptions, cleanData.ClaimData.ClaimAssumption, cleanData.ClaimData.ClaimResult, cleanData.ClaimData.Proofs})
+			writer.Flush()
+		}
 	}
 }
