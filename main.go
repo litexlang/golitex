d532f1b1d73822ac0148eb1cb3a4849e3b8102bf
malloc-realloc <malloc_realloc_free@outlook.com>
2025-10-10 09:25:13 +0800

    clean

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ import (
 )
 
 // 可以改变version的value，但是不要该VERSION这个名字，因为其他文件的grep依赖它
-const VERSION = "0.1.11-beta"
 
 func main() {
 	// Define flags
@@ -48,7 +47,7 @@ func main() {
 	}
 
 	if *versionFlag {
-		fmt.Println("Litex Kernel: golitex " + VERSION)
+		fmt.Println("Litex Kernel: golitex " + glob.VERSION)
 		return
 	}
 
