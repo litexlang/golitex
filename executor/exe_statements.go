37412643db7a507c98556a8b1eeb7df6ed43c2ca
malloc-realloc <malloc_realloc_free@outlook.com>
2025-10-21 15:33:19 +0800

    clean

--- executor/exe_statements.go
+++ executor/exe_statements.go
@@ -102,6 +102,8 @@ func (exec *Executor) Stmt(stmt ast.Stmt) (glob.ExecState, string, error) {
 		execState, err = exec.proveInRangeStmt(stmt)
 	case *ast.ProveIsTransitivePropStmt:
 		execState, err = exec.proveIsTransitivePropStmt(stmt)
+	case *ast.ProveIsCommutativePropStmt:
+		execState, err = exec.proveIsCommutativePropStmt(stmt)
 	default:
 		err = fmt.Errorf("unknown statement type: %T", stmt)
 	}
