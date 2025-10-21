e8f9cd4305eaf70c6077c8d05b20648a11f8b924
malloc-realloc <malloc_realloc_free@outlook.com>
2025-10-21 15:33:19 +0800

    clean

--- executor/exe_prove_is_commutative_prop.go
+++ executor/exe_prove_is_commutative_prop.go
@@ -1,53 +1,24 @@
 package litex_executor
 
 import (
-	"fmt"
 	ast "golitex/ast"
 	glob "golitex/glob"
-	ver "golitex/verifier"
 )
 
-func (exe *Executor) proveIsCommutativeProp(stmt *ast.SpecFactStmt) (bool, error) {
-	state := ver.Round0NoMsg
-
-	propNameAsAtom, ok := stmt.Params[0].(ast.FcAtom)
-	if !ok {
-		return false, nil
-	}
-
-	if propNameAsAtom == glob.KeySymbolEqual {
-		return true, nil
-	}
-
-	propDef, ok := exe.env.GetPropDef(propNameAsAtom)
-	if !ok {
-		return false, nil
-	}
-
-	if len(propDef.DefHeader.Params) != 2 {
-		return false, nil
-	}
-
-	uniFactParams := propDef.DefHeader.Params
-	domFacts := propDef.DomFacts
-	ThenFact := propDef.ToSpecFact()
-	IffFact, err := ThenFact.ReverseParameterOrder()
+func (exec *Executor) proveIsCommutativePropStmt(stmt *ast.ProveIsCommutativePropStmt) (glob.ExecState, error) {
+	ok, err := exec.proveIsCommutativePropStmtMainLogic(stmt)
 	if err != nil {
-		return false, err
+		return glob.ExecStateError, err
 	}
-
-	uniFact := ast.NewUniFactWithIff(ast.NewUniFact(uniFactParams, propDef.DefHeader.ParamSets, domFacts, []ast.FactStmt{ThenFact}, stmt.Line), []ast.FactStmt{IffFact}, stmt.Line)
-
-	verifier := ver.NewVerifier(exe.env)
-
-	ok, err = verifier.VerFactStmt(uniFact, state.GetNoMsg())
-	if err != nil {
-		return false, err
-	}
-
 	if ok {
-		exe.newMsg(fmt.Sprintf("%s is a commutative prop.", stmt.String()))
+		return glob.ExecStateTrue, nil
 	}
+	return glob.ExecStateUnknown, nil
+}
 
-	return ok, nil
+func (exec *Executor) proveIsCommutativePropStmtMainLogic(stmt *ast.ProveIsCommutativePropStmt) (bool, error) {
+	exec.NewEnv(exec.env)
+	defer exec.deleteEnvAndRetainMsg()
+
+	panic("not implemented")
 }
