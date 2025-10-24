d7c3b2ff1394811f1fea173e0e4d875a5040fe6c
malloc-realloc <malloc_realloc_free@outlook.com>
2025-10-24 19:26:10 +0800

    clean

--- environment/env_compute.go
+++ environment/env_compute.go
@@ -1,6 +1,7 @@
 package litex_env
 
 import (
+	"fmt"
 	ast "golitex/ast"
 	cmp "golitex/cmp"
 )
@@ -24,13 +25,24 @@ func (env *Env) CanBeComputed(fc ast.Fc) (ast.Fc, bool, error) {
 		return fc, true, nil
 	}
 
-	return nil, false, nil
-}
-
-func (comp *computer) compute(toCompute ast.Fc) (ast.Fc, bool, error) {
-	if cmp.IsNumLitFc(toCompute) {
-		return toCompute, true, nil
+	if env.IsFnWithDefinedAlgo(fc) {
+		computed, ok, err := env.Compute(fc)
+		if err != nil {
+			return nil, false, fmt.Errorf("error computing: %s", fc)
+		}
+		if ok {
+			return computed, true, nil
+		}
 	}
 
 	return nil, false, nil
+}
+
+// 算出的数值；是不是真的算出来了（因为可能没算出来，里面涉及到的符号没value什么的），出错
+func (comp *computer) compute(toCompute ast.Fc) (ast.Fc, bool, error) {
+	return nil, false, nil
+}
+
+func (env *Env) IsFnWithDefinedAlgo(fc ast.Fc) bool {
+	return false
 }
