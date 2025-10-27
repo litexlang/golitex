c4fc98c596be871b54fc006fe7a23fee176b3a90
malloc-realloc <malloc_realloc_free@outlook.com>
2025-10-27 11:23:15 +0800

    start to fix bug: make unifact params in def prop do not conflict with env

---
+++ environment/env_make_params_do_not_conflict_with_the_env.go
@@ -0,0 +1,9 @@
+package litex_env
+
+import (
+	ast "golitex/ast"
+)
+
+func (env *Env) MakeUniFactParamsInThisDefPropDoNotConflictWithEnv(defPropStmt *ast.DefPropStmt) error {
+	panic("not implemented")
+}
