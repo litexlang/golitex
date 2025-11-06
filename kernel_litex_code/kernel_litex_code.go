6cb9a1258a7451602c1e6218b02ae7d72ad98202
malloc-realloc <malloc_realloc_free@outlook.com>
2025-11-06 13:45:32 +0800

    clean

--- kernel_litex_code/kernel_litex_code.go
+++ kernel_litex_code/kernel_litex_code.go
@@ -350,7 +350,7 @@ know:
 	forall x, y, z R: x + y = z => x = z - y, y = z - x
 	forall x, y, z R: x - y = z => x = z + y, y = x - z
 	forall x, y, z R: x = y + z => x - y = z, x - z = y
-	forall x, y, z R: x = y - z => x - y = -z, x + z = y
+	forall x, y, z R: x = y - z => x - y = -z, x + z = y, z + x = y
 
 know:
 	forall a, b, c, d R: b != 0, d != 0, a / b = c / d => a * d = b * c
