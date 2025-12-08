51896d234984479bb9f59ccf32f3c55bc4ac1cce
malloc-realloc <malloc_realloc_free@outlook.com>
2025-12-08 20:35:11 +0800

    refactor:

--- executor/exe_statements.go
+++ executor/exe_statements.go
@@ -603,14 +603,11 @@ func (exec *Executor) inlineFactsStmt(stmt *ast.InlineFactsStmt) ExecRet {
 func (exec *Executor) haveObjEqualStmt(stmt *ast.HaveObjEqualStmt) ExecRet {
 	ver := NewVerifier(exec.Env)
 
-	// 证明右侧的 equal to 符合 fn 的条件
-	for _, obj := range stmt.ObjEqualTos {
-		if ver.objIsDefinedAtomOrIsFnSatisfyItsReq(obj, Round0NoMsg).IsNotTrue() {
-			return NewExecErr(fmt.Sprintf("%s is not defined or does not satisfy function requirement", obj.String()))
-		}
-	}
-
 	for i := range len(stmt.ObjNames) {
+		if ver.objIsDefinedAtomOrIsFnSatisfyItsReq(stmt.ObjEqualTos[i], Round0Msg).IsNotTrue() {
+			return NewExecErr(fmt.Sprintf("%s is not defined or does not satisfy function requirement", stmt.ObjEqualTos[i].String()))
+		}
+
 		verRet := ver.VerFactStmt(ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeywordIn), []ast.Obj{stmt.ObjEqualTos[i], stmt.ObjSets[i]}, stmt.Line), Round0Msg)
 		if verRet.IsErr() {
 			return NewExecErr(verRet.String())
