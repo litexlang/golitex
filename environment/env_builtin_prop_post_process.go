5b3620423d05d65182a56971322604faf742355b
malloc-realloc <malloc_realloc_free@outlook.com>
2025-12-12 11:40:03 +0800

    feat: post process when prop is > < <= >=

--- environment/env_builtin_prop_post_process.go
+++ environment/env_builtin_prop_post_process.go
@@ -25,20 +25,36 @@ func (env *Env) BuiltinPropExceptEqualPostProcess(fact *ast.SpecFactStmt) glob.G
 		return env.inFactPostProcess(fact)
 	}
 
-	if fact.PropName == glob.KeySymbolGreater && fact.Params[1].String() == "0" {
-		return env.builtinPropExceptEqualPostProcess_WhenPropIsGreaterAndRightParamIsZero(fact)
+	if fact.PropName == glob.KeySymbolGreater {
+		if fact.Params[1].String() == "0" {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsGreaterAndRightParamIsZero(fact)
+		} else {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsGreaterAndRightParamIsNotZero(fact)
+		}
 	}
 
-	if fact.PropName == glob.KeySymbolLargerEqual && fact.Params[1].String() == "0" {
-		return env.builtinPropExceptEqualPostProcess_WhenPropIsLargerEqualAndRightParamIsZero(fact)
+	if fact.PropName == glob.KeySymbolLargerEqual {
+		if fact.Params[1].String() == "0" {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLargerEqualAndRightParamIsZero(fact)
+		} else {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLargerEqualAndRightParamIsNotZero(fact)
+		}
 	}
 
-	if fact.PropName == glob.KeySymbolLess && fact.Params[1].String() == "0" {
-		return env.builtinPropExceptEqualPostProcess_WhenPropIsLessAndRightParamIsZero(fact)
+	if fact.PropName == glob.KeySymbolLess {
+		if fact.Params[1].String() == "0" {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLessAndRightParamIsZero(fact)
+		} else {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLessAndRightParamIsNotZero(fact)
+		}
 	}
 
-	if fact.PropName == glob.KeySymbolLessEqual && fact.Params[1].String() == "0" {
-		return env.builtinPropExceptEqualPostProcess_WhenPropIsLessEqualAndRightParamIsZero(fact)
+	if fact.PropName == glob.KeySymbolLessEqual {
+		if fact.Params[1].String() == "0" {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLessEqualAndRightParamIsZero(fact)
+		} else {
+			return env.builtinPropExceptEqualPostProcess_WhenPropIsLessEqualAndRightParamIsNotZero(fact)
+		}
 	}
 
 	return glob.NewGlobUnknown("")
@@ -219,6 +235,230 @@ func (env *Env) builtinPropExceptEqualPostProcess_WhenPropIsLessEqualAndRightPar
 	// -x >= 0
 	minusXGreaterEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{minusX, ast.Atom("0")}, fact.Line)
 	ret = env.storeSpecFactInMem(minusXGreaterEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	return glob.NewGlobTrue("")
+}
+
+func (env *Env) builtinPropExceptEqualPostProcess_WhenPropIsGreaterAndRightParamIsNotZero(fact *ast.SpecFactStmt) glob.GlobRet {
+	// x > c (c != 0)
+	// x != c
+	notEqualCFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolNotEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret := env.storeSpecFactInMem(notEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x >= c
+	greaterEqualCFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret = env.storeSpecFactInMem(greaterEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not x <= c
+	lessEqualCFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret = env.storeSpecFactInMem(lessEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c < x (等价表述)
+	cLessThanXFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLess), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cLessThanXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not c >= x
+	cGreaterEqualXFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cGreaterEqualXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c > 0
+	xMinusC := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[0], fact.Params[1]})
+	xMinusCGreaterThanZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolGreater), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCGreaterThanZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c >= 0
+	xMinusCGreaterEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCGreaterEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x < 0
+	cMinusX := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[1], fact.Params[0]})
+	cMinusXLessThanZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLess), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXLessThanZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x <= 0
+	cMinusXLessEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXLessEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	return glob.NewGlobTrue("")
+}
+
+func (env *Env) builtinPropExceptEqualPostProcess_WhenPropIsLargerEqualAndRightParamIsNotZero(fact *ast.SpecFactStmt) glob.GlobRet {
+	// x >= c (c != 0)
+	// not x < c
+	lessCFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLess), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret := env.storeSpecFactInMem(lessCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c <= x (等价表述)
+	cLessEqualXFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cLessEqualXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not c > x
+	cGreaterXFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolGreater), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cGreaterXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c >= 0
+	xMinusC := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[0], fact.Params[1]})
+	xMinusCGreaterEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCGreaterEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x <= 0
+	cMinusX := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[1], fact.Params[0]})
+	cMinusXLessEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXLessEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	return glob.NewGlobTrue("")
+}
+
+func (env *Env) builtinPropExceptEqualPostProcess_WhenPropIsLessAndRightParamIsNotZero(fact *ast.SpecFactStmt) glob.GlobRet {
+	// x < c (c != 0)
+	// x != c
+	notEqualCFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolNotEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret := env.storeSpecFactInMem(notEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x <= c
+	lessEqualCFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret = env.storeSpecFactInMem(lessEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not x >= c
+	greaterEqualCFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret = env.storeSpecFactInMem(greaterEqualCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c > x (等价表述)
+	cGreaterThanXFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolGreater), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cGreaterThanXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not c <= x
+	cLessEqualXFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cLessEqualXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c < 0
+	xMinusC := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[0], fact.Params[1]})
+	xMinusCLessThanZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLess), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCLessThanZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c <= 0
+	xMinusCLessEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCLessEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x > 0
+	cMinusX := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[1], fact.Params[0]})
+	cMinusXGreaterThanZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolGreater), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXGreaterThanZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x >= 0
+	cMinusXGreaterEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXGreaterEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	return glob.NewGlobTrue("")
+}
+
+func (env *Env) builtinPropExceptEqualPostProcess_WhenPropIsLessEqualAndRightParamIsNotZero(fact *ast.SpecFactStmt) glob.GlobRet {
+	// x <= c (c != 0)
+	// not x > c
+	greaterCFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolGreater), []ast.Obj{fact.Params[0], fact.Params[1]}, fact.Line)
+	ret := env.storeSpecFactInMem(greaterCFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c >= x (等价表述)
+	cGreaterEqualXFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cGreaterEqualXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// not c < x
+	cLessXFact := ast.NewSpecFactStmt(ast.FalsePure, ast.Atom(glob.KeySymbolLess), []ast.Obj{fact.Params[1], fact.Params[0]}, fact.Line)
+	ret = env.storeSpecFactInMem(cLessXFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// x - c <= 0
+	xMinusC := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[0], fact.Params[1]})
+	xMinusCLessEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLessEqual), []ast.Obj{xMinusC, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(xMinusCLessEqualZeroFact)
+	if ret.IsErr() {
+		return ret
+	}
+
+	// c - x >= 0
+	cMinusX := ast.NewFnObj(ast.Atom(glob.KeySymbolMinus), []ast.Obj{fact.Params[1], fact.Params[0]})
+	cMinusXGreaterEqualZeroFact := ast.NewSpecFactStmt(ast.TruePure, ast.Atom(glob.KeySymbolLargerEqual), []ast.Obj{cMinusX, ast.Atom("0")}, fact.Line)
+	ret = env.storeSpecFactInMem(cMinusXGreaterEqualZeroFact)
 	if ret.IsErr() {
 		return ret
 	}
