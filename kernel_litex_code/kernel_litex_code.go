5a346983351c62c177b61b15f6f6def64a3bf6f9
malloc-realloc <malloc_realloc_free@outlook.com>
2026-01-10 10:17:17 +0800

    feat: product_of

--- kernel_litex_code/kernel_litex_code.go
+++ kernel_litex_code/kernel_litex_code.go
@@ -1007,5 +1007,11 @@ know:
         =>:
             exist z R st $is_min(z, s)
 
-
+prop is_prime(x N_pos):
+	x != 1
+    forall y N_pos:
+        2 <= y
+        y < x
+        =>:
+            x % y != 0	
 `
