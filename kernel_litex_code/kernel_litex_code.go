5a37597b6bb6676fade8f63a3bf116e95fe1e92d
malloc-realloc <malloc_realloc_free@outlook.com>
2026-01-17 12:01:34 +0800

    feat: pigeon hole

--- kernel_litex_code/kernel_litex_code.go
+++ kernel_litex_code/kernel_litex_code.go
@@ -1082,6 +1082,7 @@ know forall x, y Z: x <= y => count(range(x, y)) = y - x
 know forall x, y Z: x <= y => count(closed_range(x, y)) = y - x + 1
 know forall s1, s2 finite_set: count(s1) = count(s2) <=> exist z fn(s1)s2 st $is_bijective_fn(s1, s2, z)
 know forall s1, s2 finite_set, f fn(s1)s2: count(s1) = count(s2) => $is_injective_fn(s1, s2, f) <=> $is_bijective_fn(s1, s2, f)
+know forall s1, s2 finite_set: count(s1) >= count(s2) <=> exist f fn(s1)s2 st $is_injective_fn(s1, s2, f)
 
 know forall x finite_set, y set: set_minus(x, y) $is_finite_set
 `
