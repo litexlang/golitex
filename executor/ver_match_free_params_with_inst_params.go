5e7a107a6ea0cd8ea25caa9b48565e75da6e4018
malloc-realloc <malloc_realloc_free@outlook.com>
2026-01-23 10:36:11 +0800

    feat: clean

---
+++ executor/ver_match_free_params_with_inst_params.go
@@ -0,0 +1,21 @@
+// Copyright Jiachen Shen.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Original Author: Jiachen Shen <malloc_realloc_free@outlook.com>
+// Litex email: <litexlang@outlook.com>
+// Litex website: https://litexlang.com
+// Litex github repository: https://github.com/litexlang/golitex
+// Litex Zulip community: https://litex.zulipchat.com/join/c4e7foogy6paz2sghjnbujov/
+
+package litex_executor
+
+import ast "golitex/ast"
+
+func (ver *Verifier) verMatchFreeParamsWithInstParams(freeParams []string, instParams []ast.Obj) (bool, map[string]ast.Obj, error) {
+	panic("not implemented")
+}
