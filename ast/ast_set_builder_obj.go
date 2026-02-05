93f2f0157d533401b3445ccc499d13f897cbda71
malloc-realloc <malloc_realloc_free@outlook.com>
2026-02-05 11:08:35 +0800

    feat: know xxx in fn sort of workss

--- ast/ast_set_builder_obj.go
+++ ast/ast_set_builder_obj.go
@@ -3,7 +3,7 @@ package litex_ast
 type SetBuilderObj struct {
 }
 
-func (obj *SetBuilderObj) obj()
-func (obj *SetBuilderObj) String() string
-func (obj *SetBuilderObj) Instantiate(map[string]Obj) (Obj, error)
-func (obj *SetBuilderObj) ToLatexString() string
+func (obj *SetBuilderObj) obj()                                    {}
+func (obj *SetBuilderObj) String() string                          { return "" }
+func (obj *SetBuilderObj) Instantiate(map[string]Obj) (Obj, error) { return nil, nil }
+func (obj *SetBuilderObj) ToLatexString() string                   { return "" }
