3533e1a59b89ab5f635e00a6407fcba78e17266d
malloc-realloc <malloc_realloc_free@outlook.com>
2026-02-09 19:03:49 +0800

    feat: update algo

--- executor/exe_algo_def.go
+++ executor/exe_algo_def.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	ast "golitex/ast"
 	litex_env "golitex/environment"
+	glob "golitex/glob"
 )
 
 func (exec *Executor) defAlgoStmt(stmt *ast.DefAlgoStmt) ast.StmtRet {
@@ -30,6 +31,13 @@ func (exec *Executor) checkDefAlgoStmt(stmt *ast.DefAlgoStmt) ast.VerRet {
 	}
 
 	// 按def里的param的定义来定义param
+	ret := ver.defineParamsInAlgoDef(stmt, def)
+	if ret.IsNotTrue() {
+		return ret
+	}
+
+	// 在每个if 下面检查是不是OK
+	panic("")
 
 	return ast.NewTrueVerRet(nil, nil, "")
 }
@@ -46,10 +54,47 @@ func (ver *Verifier) defineParamsInAlgoDef(stmt *ast.DefAlgoStmt, fnSetObj ast.F
 }
 
 func (ver *Verifier) defineParamsInFnSetObjWithoutName(stmt *ast.DefAlgoStmt, fnSetObj *ast.FnSetObjWithoutName) ast.VerRet {
-	// defLetStmt := ast.NewDefLetStmt(stmt.Params, fnSetObj.ParamSets, fnSetObj.DomFacts.ToFactStmtSlice(), glob.BuiltinLine0)
-	panic("")
+	defLetStmt := ast.NewDefLetStmt(stmt.Params, fnSetObj.ParamSets, []ast.FactStmt{}, glob.BuiltinLine0)
+	ret := ver.Env.DefLetStmt(defLetStmt)
+	if ret.IsNotTrue() {
+		return ast.NewErrVerRet(nil).AddExtraInfo(ret.String())
+	}
+	return ast.NewTrueVerRet(nil, nil, "")
 }
 
 func (ver *Verifier) defineParamsInFnSetObjWithName(stmt *ast.DefAlgoStmt, fnSetObj *ast.FnSetObjWithName) ast.VerRet {
-	panic("	")
+	if len(stmt.Params) != len(fnSetObj.Params) {
+		return ast.NewErrVerRet(nil).AddExtraInfo(fmt.Sprintf("parameter length mismatch: %d != %d", len(stmt.Params), len(fnSetObj.Params)))
+	}
+
+	uniMap := make(map[string]ast.Obj)
+
+	for i, param := range stmt.Params {
+		uniMap[fnSetObj.Params[i]] = ast.Atom(param)
+	}
+
+	paramSets := make([]ast.Obj, len(stmt.Params))
+	for i, paramSet := range fnSetObj.ParamSets {
+		newParamSet, err := paramSet.Instantiate(uniMap)
+		if err != nil {
+			return ast.NewErrVerRet(nil).AddExtraInfo(err.Error())
+		}
+		paramSets[i] = newParamSet
+	}
+
+	domFacts := make([]ast.FactStmt, len(fnSetObj.DomFacts))
+	for i, domFact := range fnSetObj.DomFacts {
+		newDomFact, err := domFact.Instantiate(uniMap)
+		if err != nil {
+			return ast.NewErrVerRet(nil).AddExtraInfo(err.Error())
+		}
+		domFacts[i] = newDomFact.(ast.FactStmt)
+	}
+
+	defLetStmt := ast.NewDefLetStmt(stmt.Params, paramSets, domFacts, glob.BuiltinLine0)
+	ret := ver.Env.DefLetStmt(defLetStmt)
+	if ret.IsNotTrue() {
+		return ast.NewErrVerRet(nil).AddExtraInfo(ret.String())
+	}
+	return ast.NewTrueVerRet(nil, nil, "")
 }
