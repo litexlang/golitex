5976cf92d68e37cdf49f492c7b62a5abc0404972
malloc-realloc <malloc_realloc_free@outlook.com>
2026-03-02 17:04:19 +0800

    feat: start to implement module manager in rustlitex

--- litex/src/runtime_context.rs
+++ litex/src/runtime_context.rs
@@ -1,8 +1,5 @@
-pub struct RuntimeContext {
-    pub module_manager: ModuleManager,
-    pub env_stack: Vec<Env>,
-    pub objs: HashMap<String, ()>,
-    pub props: HashMap<String, DefPropStmt>,
-    pub algos: HashMap<String, DefAlgoStmt>,
-    pub set_templates: HashMap<String, DefSetTemplateStmt>,
+use crate::module_manager::ModuleManager;
+
+pub struct RuntimeContext<'a> {
+    pub module_manager: &'a ModuleManager<'a>,
 }
