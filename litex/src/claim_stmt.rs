626cf36991f185bc213ed2712c93917bf5107e44
malloc-realloc <malloc_realloc_free@outlook.com>
2026-03-05 11:36:47 +0800

    feat: clean

--- litex/src/claim_stmt.rs
+++ litex/src/claim_stmt.rs
@@ -1,7 +1,7 @@
 use std::fmt;
+use crate::forall_fact_with_iff::ForallFactWithIff;
 use crate::keywords::{CLAIM, COLON, PROVE};
 use crate::fact::Fact;
-use crate::forall_fact::ForallFact;
 use crate::helper::{to_string_and_add_four_spaces_at_beginning_of_each_line,  vec_to_string_add_four_spaces_at_beginning_of_each_line, add_four_spaces_at_beginning};
 use crate::stmt::Stmt;
 
@@ -11,33 +11,33 @@ pub enum ClaimStmt {
 }
 
 pub struct ClaimIffStmt {
-    pub to_prove: ForallFact,
+    pub to_prove: ForallFactWithIff,
     pub then_to_iff_proof: Vec<Stmt>,
     pub iff_to_then_proof: Vec<Stmt>,
     pub line_file_index: Option<(usize, usize)>,
 }
 
 impl ClaimIffStmt {
-    pub fn new(to_prove: ForallFact, then_to_iff_proof: Vec<Stmt>, iff_to_then_proof: Vec<Stmt>, line_file_index: Option<(usize, usize)>) -> Self {
+    pub fn new(to_prove: ForallFactWithIff, then_to_iff_proof: Vec<Stmt>, iff_to_then_proof: Vec<Stmt>, line_file_index: Option<(usize, usize)>) -> Self {
         ClaimIffStmt { to_prove, then_to_iff_proof, iff_to_then_proof, line_file_index }
     }
 }
 
 pub struct ClaimProveStmt {
-    pub to_prove: Fact,
+    pub to_prove: Vec<Fact>,
     pub proof: Vec<Stmt>,
     pub line_file_index: Option<(usize, usize)>,
 }
 
 impl ClaimProveStmt {
-    pub fn new(to_prove: Fact, proof: Vec<Stmt>, line_file_index: Option<(usize, usize)>) -> Self {
+    pub fn new(to_prove: Vec<Fact>, proof: Vec<Stmt>, line_file_index: Option<(usize, usize)>) -> Self {
         ClaimProveStmt { to_prove, proof, line_file_index }
     }
 }
 
 impl fmt::Display for ClaimProveStmt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}\n{}\n{}{}\n{}", CLAIM, COLON,to_string_and_add_four_spaces_at_beginning_of_each_line(&self.to_prove, 1), add_four_spaces_at_beginning(PROVE, 1), COLON, vec_to_string_add_four_spaces_at_beginning_of_each_line(&self.proof, 2))
+        write!(f, "{}{}\n{}\n{}{}\n{}", CLAIM, COLON, vec_to_string_add_four_spaces_at_beginning_of_each_line(&self.to_prove, 1), add_four_spaces_at_beginning(PROVE, 1), COLON, vec_to_string_add_four_spaces_at_beginning_of_each_line(&self.proof, 2))
     }
 }
 
