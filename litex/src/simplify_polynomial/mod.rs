ef6f725fdcc7b463ac20fc5a083d224fbfe094b9
malloc-realloc <malloc_realloc_free@outlook.com>
2026-03-19 10:54:34 +0800

    feat: checkpoint: can check a / b + c /d = (a * d + c * b) / (b *d) automatically

--- litex/src/simplify_polynomial/mod.rs
+++ litex/src/simplify_polynomial/mod.rs
@@ -2,8 +2,9 @@ mod calculate;
 mod collect_monomials;
 mod monomial;
 mod simplify_polynomial;
+mod process_division_after_polynomial_simplification;
 
 #[cfg(test)]
 mod simplify_polynomial_test;
 
-pub use simplify_polynomial::two_objs_equal_by_polynomial_simplification;
+pub use simplify_polynomial::two_objs_equal_by_polynomial_simplification_and_division_process;
