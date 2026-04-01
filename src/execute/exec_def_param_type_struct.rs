ddf973225f895496ce85c60075bc25a473813980
malloc-realloc <malloc_realloc_free@outlook.com>
2026-04-01 13:43:42 +0800

    feat(new): start to implement define param struct
    Why this update matters:
    - New feature: add new functionality or improve existing capabilities.

--- src/execute/exec_def_param_type_struct.rs
+++ src/execute/exec_def_param_type_struct.rs
@@ -5,8 +5,7 @@ impl Runtime {
         &mut self,
         def_param_type_struct_stmt: &DefParamTypeStructStmt,
     ) -> Result<NonErrStmtExecResult, ExecStmtError> {
-        let struct_definition_infer_result =
-            self.def_param_type_struct_stmt_check_well_defined(def_param_type_struct_stmt)?;
+        self.def_param_type_struct_stmt_check_well_defined(def_param_type_struct_stmt)?;
 
         self.store_def_param_type_struct(def_param_type_struct_stmt)
             .map_err(|store_error| {
@@ -18,19 +17,19 @@ impl Runtime {
                 )
             })?;
 
-        Ok(NonErrStmtExecResult::NonFactualStmtSuccess(
-            NonFactualStmtSuccess::new(
-                Stmt::DefParamTypeStructStmt(def_param_type_struct_stmt.clone()),
-                struct_definition_infer_result,
-                vec![],
-            ),
-        ))
+        let infer_result = InferResult::new();
+
+        Ok(NonErrStmtExecResult::NonFactualStmtSuccess(NonFactualStmtSuccess::new(
+            Stmt::DefParamTypeStructStmt(def_param_type_struct_stmt.clone()),
+            infer_result,
+            vec![],
+        )))
     }
 
     fn def_param_type_struct_stmt_check_well_defined(
         &mut self,
         def_param_type_struct_stmt: &DefParamTypeStructStmt,
-    ) -> Result<InferResult, ExecStmtError> {
+    ) -> Result<(), ExecStmtError> {
         self.push_env();
         let struct_check_well_defined_result = self
             .def_param_type_struct_stmt_check_well_defined_body(def_param_type_struct_stmt);
@@ -40,8 +39,52 @@ impl Runtime {
 
     fn def_param_type_struct_stmt_check_well_defined_body(
         &mut self,
-        _def_param_type_struct_stmt: &DefParamTypeStructStmt,
-    ) -> Result<InferResult, ExecStmtError> {
-        unimplemented!()
+        def_param_type_struct_stmt: &DefParamTypeStructStmt,
+    ) -> Result<(), ExecStmtError> {
+        let verify_state = VerifyState::new(0, false);
+
+        self
+            .define_params_with_type(&def_param_type_struct_stmt.params_def_with_type, false)
+            .map_err(|define_params_error| {
+                ExecStmtError::new(
+                    Stmt::DefParamTypeStructStmt(def_param_type_struct_stmt.clone()),
+                    "".to_string(),
+                    Some(define_params_error.into()),
+                    vec![],
+                )
+            })?;
+
+        for (_, field_obj) in def_param_type_struct_stmt.fields.iter() {
+            self.verify_obj_well_defined_and_store_cache(field_obj, &verify_state)
+                .map_err(|well_defined_error| {
+                    ExecStmtError::new(
+                        Stmt::DefParamTypeStructStmt(def_param_type_struct_stmt.clone()),
+                        "".to_string(),
+                        Some(well_defined_error.into()),
+                        vec![],
+                    )
+                })?;
+        }
+
+        // 在这里申明 field access
+        // self 在 inst_struct
+
+        for fact in def_param_type_struct_stmt.facts.iter() {
+            self
+                .verify_or_and_chain_atomic_fact_well_defined_and_store_and_infer(
+                    fact,
+                    &verify_state,
+                )
+                .map_err(|inner_exec_error| {
+                    ExecStmtError::new(
+                        Stmt::DefParamTypeStructStmt(def_param_type_struct_stmt.clone()),
+                        "".to_string(),
+                        Some(RuntimeError::ExecStmtError(inner_exec_error)),
+                        vec![],
+                    )
+                })?;
+        }
+
+        Ok(())
     }
 }
