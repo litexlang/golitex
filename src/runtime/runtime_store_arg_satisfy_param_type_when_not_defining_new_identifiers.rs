7207da820d22d885ccf1197d5f94bf5db3eb5110
malloc-realloc <malloc_realloc_free@outlook.com>
2026-04-07 16:58:40 +0800

    chore(clean): clean store arg satisfy param type bug
    Why this update matters:
    - Clean: reduce clutter and simplify code paths for easier maintenance.

---
+++ src/runtime/runtime_store_arg_satisfy_param_type_when_not_defining_new_identifiers.rs
@@ -0,0 +1,48 @@
+use crate::prelude::*;
+
+impl Runtime {
+    pub fn store_args_satisfy_param_type_when_not_defining_new_identifiers(
+        &mut self,
+        param_defs: &Vec<ParamGroupWithParamType>,
+        args: &Vec<Obj>,
+        _line_file: LineFile,
+    ) -> Result<InferResult, RuntimeError> {
+        let instantiated_types = self.inst_param_def_with_type_one_by_one(param_defs, args)?;
+
+        let mut infer_result = InferResult::new();
+        for (arg, param_type) in args.iter().zip(instantiated_types.iter()) {
+            let new_fact: Fact = match param_type {
+                ParamType::Set(_) => Fact::AtomicFact(AtomicFact::IsSetFact(IsSetFact::new(
+                    arg.clone(),
+                    default_line_file(),
+                ))),
+                ParamType::NonemptySet(_) => Fact::AtomicFact(AtomicFact::IsNonemptySetFact(
+                    IsNonemptySetFact::new(arg.clone(), default_line_file()),
+                )),
+                ParamType::FiniteSet(_) => Fact::AtomicFact(AtomicFact::IsFiniteSetFact(
+                    IsFiniteSetFact::new(arg.clone(), default_line_file()),
+                )),
+                ParamType::Obj(obj) => Fact::AtomicFact(AtomicFact::InFact(InFact::new(
+                    arg.clone(),
+                    obj.clone(),
+                    default_line_file(),
+                ))),
+                ParamType::Family(family) => Fact::AtomicFact(AtomicFact::InFact(InFact::new(
+                    arg.clone(),
+                    Obj::FamilyObj(family.clone()),
+                    default_line_file(),
+                ))),
+                ParamType::Struct(struct_ty) => Fact::AtomicFact(AtomicFact::InFact(InFact::new(
+                    arg.clone(),
+                    Obj::StructObj(struct_ty.clone()),
+                    default_line_file(),
+                ))),
+            };
+            infer_result.new_infer_result_inside(
+                self.store_fact_without_well_defined_verified_and_infer(new_fact)?,
+            );
+        }
+
+        Ok(infer_result)
+    }
+}
