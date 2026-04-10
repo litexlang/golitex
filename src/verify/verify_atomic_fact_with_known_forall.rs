b23635ee2e185c390c86e1f6bd5643d5ffb387da
malloc-realloc <malloc_realloc_free@outlook.com>
2026-04-10 15:08:19 +0800

    chore(clean): examples
    Why this update matters:
    - Clean: reduce clutter and simplify code paths for easier maintenance.

--- src/verify/verify_atomic_fact_with_known_forall.rs
+++ src/verify/verify_atomic_fact_with_known_forall.rs
@@ -184,13 +184,14 @@ impl Runtime {
             )],
             known_forall.line_file.clone(),
         );
-        let fact_verified = FactualStmtSuccess::new_with_verified_by_known_fact_source_recording_facts(
-            Fact::AtomicFact(given_atomic_fact.clone()),
-            verified_by_known_forall_fact.to_string(),
-            Some(Fact::ForallFact(verified_by_known_forall_fact.clone())),
-            None,
-            Vec::new(),
-        );
+        let fact_verified =
+            FactualStmtSuccess::new_with_verified_by_known_fact_source_recording_facts(
+                Fact::AtomicFact(given_atomic_fact.clone()),
+                verified_by_known_forall_fact.to_string(),
+                Some(Fact::ForallFact(verified_by_known_forall_fact.clone())),
+                None,
+                Vec::new(),
+            );
         Ok(Some(fact_verified))
     }
 
