5a78970eb833d09ca2591bf54f2f71917df34d53
malloc-realloc <malloc_realloc_free@outlook.com>
2026-04-13 20:00:39 +0800

    feat(new): more buitlin comparsion rule
    Why this update matters:
    - New feature: add new functionality or improve existing capabilities.

--- src/runtime/builtin_code/common_comparison_properties.rs
+++ src/runtime/builtin_code/common_comparison_properties.rs
@@ -1,5 +1,6 @@
 // Common comparison properties: trichotomy and witnesses on R, zero-product, transitivity
-// (props + know), and order closure under +, -, *, / (conclusions use only <= and <).
+// (props + know), and difference characterization (`a - b` vs `0`).
+// Order closure under +, -, *, / on general inequalities is in `order_algebra_builtin.rs`.
 
 pub const KNOW_REAL_LINE_TRICHOTOMY: &str = r#"
 know:
@@ -92,100 +93,6 @@ know:
 
 pub const BUILTIN_ENV_CODE_FOR_COMMON_COMPARISON_PROPERTIES: &str = r#"
 know:
-    forall a, b, x R:
-        0 <= x
-        a <= b
-        =>:
-            a * x <= b * x
-            x * a <= x * b
-
-    forall a, b, x R:
-        x <= 0
-        a <= b
-        =>:
-            b * x <= a * x
-            x * b <= x * a
-
-    forall a, b, c, d R:
-        a <= b
-        c <= d
-        =>:
-            a + c <= b + d
-
-    forall a, b, c, d R:
-        a <= b
-        c <= d
-        =>:
-            a - d <= b - c
-
-    forall a, b, c, d R:
-        a < b
-        c < d
-        =>:
-            a + c < b + d
-
-    forall a, b, c, d R:
-        a <= b
-        c < d
-        =>:
-            a + c < b + d
-
-    forall a, b, c, d R:
-        a < b
-        c <= d
-        =>:
-            a + c < b + d
-
-    forall a, b, x R:
-        0 < x
-        a < b
-        =>:
-            a * x < b * x
-            x * a < x * b
-
-    forall a, b, x R:
-        0 < x
-        a <= b
-        =>:
-            a * x <= b * x
-            x * a <= x * b
-
-    forall a, b, x R:
-        x < 0
-        a < b
-        =>:
-            b * x < a * x
-
-    forall a, b, x R:
-        x < 0
-        a <= b
-        =>:
-            b * x <= a * x
-
-    forall a, b, c R:
-        0 < c
-        a <= b
-        =>:
-            a / c <= b / c
-
-    forall a, b, c R:
-        0 < c
-        a < b
-        =>:
-            a / c < b / c
-
-    forall a, b, c R:
-        c < 0
-        a <= b
-        =>:
-            b / c <= a / c
-
-    forall a, b, c R:
-        c < 0
-        a < b
-        =>:
-            b / c < a / c
-
     forall a, b R:
         a <= b
         =>:
