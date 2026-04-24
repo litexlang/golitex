21539cdf55f05e414e6c2e4af3a15df2932f0020
malloc-realloc <malloc_realloc_free@outlook.com>
2026-04-24 10:06:14 +0800

    refactor(impl): builtin function
    Why this update matters:
    - Change implementation: rework internals (structure, algorithms, or data representation) while preserving intended behavior.

--- src/builtin_code/common_functions.rs
+++ src/builtin_code/common_functions.rs
@@ -6,4 +6,21 @@ know:
 
     forall x, y R:
         abs(x * y) = abs(x) * abs(y)
+
+# Existence of such function is valid by definition of comparison
+let max_of_finite_set fn(s power_set(R): $is_finite_set(s)) R
+let min_of_finite_set fn(s power_set(R): $is_finite_set(s)) R
+
+know:
+    forall s power_set(R), item s:
+        $is_finite_set(s)
+        =>:
+            item <= max_of_finite_set(s)
+            min_of_finite_set(s) <= item
+
+    forall s power_set(R):
+        $is_finite_set(s)
+        =>:
+            max_of_finite_set(s) $in s
+            min_of_finite_set(s) $in s
 "#;
